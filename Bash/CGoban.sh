47a5904174c1eb6a318e178d21f436104d35a978
Guillaume Raimondeau <raimondeau@gmail.com>
2014-03-17 15:16:30 +0100

    new file:   Bash/CGoban.sh 	new file:   Bash/_bashrc_home

---
+++ Bash/CGoban.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/opt/java/bin/javaws /home/guillaume/cgoban-nfa.jnlp
+
+
+#export JAVAWS_HOME=/usr/bin/javaws
+#wget "http://files.gokgs.com/javaBin/cgoban.jnlp"
+#javaws cgoban.jnlp &
