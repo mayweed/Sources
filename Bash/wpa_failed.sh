f2b2cc95e9ad206bbb074a3a291c2e7482487108
Guillaume R <klingon357@gmail.com>
2015-11-02 19:46:50 +0100

    I do not need that anymore

--- Bash/wpa_failed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# ameliorer le harcode de l'interface, verifier que usage est root
-# recuperer les messages d'erreur dans des logs
-ifconfig eth0 down
-ifconfig ethO up
-wpa_supplicant -B -Dwext -i eth0 -c /etc/wpa_supplicant.conf
-dhcpcd eth0
