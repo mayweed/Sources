2c6b46a3f3ed20ea652665c2aaed1b5797341306
Guillaume Raimondeau <raimondeau@gmail.com>
2016-03-10 10:57:41 +0100

    Reminder of set operators

--- Python/graph_1_GLMF.py
+++ Python/graph_1_GLMF.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
+# Set operators:
+# &: letters in both operands
+# ^: letters either op but NOT in both
+# -: letters in the left op but NOT in the right op
+# |: letters in either op or both
+
 # A dict in dict to represent the graph
 M={"A":{"B","D"},
    "B":{"A","F","G","H"},
@@ -10,17 +16,21 @@ M={"A":{"B","D"},
    "G":{"B","H"},
    "H":{"B","E","F","G"}} 
 
-# Curly braces or the set() function can be used to create sets.(PyTut)
+# Curly braces or the set() function can be used to create sets.
+#(PyTut 5.4)
 afaire={"G"}
+# Note: to create an empty set you have to use set(), not {}
+# (PyTut 5.4)
 fait=set()
 
 while afaire:
     temp=set()
     for s in afaire:
         temp|=M[s]
-    print(temp)
     temp-=fait
+    # - in set: letters in temp but NOT in afaire
     temp-=afaire
+    # | in set: letters in either fait or afaire
     fait|=afaire
     afaire=set(temp)
     print(temp)
