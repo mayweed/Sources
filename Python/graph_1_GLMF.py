4c2a765299198fb2819e7a41d5b5d8c48fa3c8d2
Guillaume Raimondeau <raimondeau@gmail.com>
2016-03-10 10:21:12 +0100

    some comments

--- Python/graph_1_GLMF.py
+++ Python/graph_1_GLMF.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# A dict in dict to represent the graph
 M={"A":{"B","D"},
    "B":{"A","F","G","H"},
    "C":{"E","F"},
@@ -8,6 +9,8 @@ M={"A":{"B","D"},
    "F":{"B","C","E","H"},
    "G":{"B","H"},
    "H":{"B","E","F","G"}} 
+
+# Curly braces or the set() function can be used to create sets.(PyTut)
 afaire={"G"}
 fait=set()
 
@@ -15,6 +18,7 @@ while afaire:
     temp=set()
     for s in afaire:
         temp|=M[s]
+    print(temp)
     temp-=fait
     temp-=afaire
     fait|=afaire
