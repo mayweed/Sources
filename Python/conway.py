e3174b6b0d9c018a2a659a2aa57179962e0a8495
Guillaume Raimondeau <raimondeau@gmail.com>
2016-05-21 17:15:14 +0200

    Think!!Stop writing just think!!

--- Python/conway.py
+++ Python/conway.py
@@ -9,14 +9,24 @@ def conway(L):
     if len(L)==1:result=[1,L[0]]
     else:
          prev=L[0]        
-         for i in L[1:]:
+         for i in in range (len L[1:]):
             #Do not forget the end of a list
-            if i==prev:
+            if L[i]==prev:
                 count+=1
                 result.append([count,i]) 
             if i!=prev:
-                result.append([count,i]) 
+                result.append([count,i])
                 count=1
+                prev=i
     return(result)
 
-print(conway([2,1]))
+#for i in range(5):
+#    print(conway([2]))
+
+print(conway([3,2,1,1,1,1]))
+#Should implement recursively
+# base case: len==1 ==>1
+# if len > 1:
+# read the line char by char
+# if one is alone of is kind: base case
+# else count len and output final_count(len) i
