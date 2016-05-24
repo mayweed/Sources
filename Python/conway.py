4608b65088938b285429a773cfc257d1f4253d8b
Guillaume Raimondeau <raimondeau@gmail.com>
2016-05-24 14:35:53 +0200

    Not entirely satisfied, would prefer one w/o split() for ex...

--- Python/conway.py
+++ Python/conway.py
@@ -2,28 +2,36 @@
 
 def conway(L):
     """
-    takes a string:say-as-you-read
+    takes a int:say-as-you-read
     """
     count=1
-    result=" "
-    prev=L[0]        
+    result=""
+    L=L.split()
+    prev=L[0]
+
     for i in L[1:]:
         #skip the spaces
         if i==" ":continue
         else:
             if i!=prev:
-                result+=str(count)+" "+prev+" "
+                result+=str(count)+" "+str(prev)+" "
                 count=1
                 prev=i
             elif i==prev:
                 count+=1
 
     # At the end of the string... or if string==1
-    result+=str(count)+" "+prev+" "
+    result+=str(count)+" "+str(prev)
 
     return(result)
 
 #for i in range(5):
 #    print(conway("2"))
 
-print(conway("3"))
+#print(conway("1 1 25"))
+R="25"
+L=9
+for x in range(L):
+    res=conway(R)
+    R=res
+    print(res)
