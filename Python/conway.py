6e5ccd976b4d0a06a100408208db0bfeadec52f5
Guillaume Raimondeau <raimondeau@gmail.com>
2016-05-24 09:13:29 +0200

    some modifs: parse a string not a list etc...

--- Python/conway.py
+++ Python/conway.py
@@ -2,31 +2,24 @@
 
 def conway(L):
     """
-    takes a list:say-as-you-read
+    takes a string:say-as-you-read
     """
     count=1
-    result=[]
-    if len(L)==1:result=[1,L[0]]
-    else:
-         prev=L[0]        
-         for i in in range (len L[1:]):
-            #Do not forget the end of a list
-            if L[i]==prev:
-                count+=1
-                result.append([count,i]) 
+    result=" "
+    prev=L[0]        
+    for i in L[1:]:
+        #skip the spaces
+        if i==" ":continue
+        else:
             if i!=prev:
-                result.append([count,i])
+                result+=str(count)+" "+prev+" "
                 count=1
                 prev=i
+            elif i==prev:
+                count+=1
     return(result)
 
 #for i in range(5):
 #    print(conway([2]))
 
-print(conway([3,2,1,1,1,1]))
-#Should implement recursively
-# base case: len==1 ==>1
-# if len > 1:
-# read the line char by char
-# if one is alone of is kind: base case
-# else count len and output final_count(len) i
+print(conway("3 2 1 1 1"))
