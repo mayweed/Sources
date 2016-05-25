00ba3379e553e942b0a4da238c59a3f41d085eae
Guillaume Raimondeau <raimondeau@gmail.com>
2016-05-25 11:10:52 +0200

    conway

---
+++ Go/conway.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+import "strings"
+
+func conway(L string) string{
+    var count int =1
+    var result string=""
+    var Lsplit []string
+    Lsplit=strings.Split(L," ")
+    prev:=Lsplit[0]
+    for i:=1; i<len(Lsplit);i++{
+        if Lsplit[i]==" "{
+            continue
+        }else{
+            if Lsplit[i]!=prev{
+                result+=string(count)+" "+prev+" "
+                count=1
+                prev=Lsplit[i]
+            }else if Lsplit[i]==prev{
+                count+=1
+            }
+        }
+    }
+    result+=string(count)+" "+prev
+
+    return result
+}
+
+
+func main() {
+    var R string
+    fmt.Scan(&R)
+
+    var L int
+    fmt.Scan(&L)
+
+    var res string
+    for x:=0;x<L-1;x++{
+        res=conway(R)
+        R=res
+    }
+    fmt.Println(R)
+    // fmt.Fprintln(os.Stderr, "Debug messages...")
+}
