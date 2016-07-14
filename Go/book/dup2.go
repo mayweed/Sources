6554e0822c24cbe844e45c3ff8014e24b4a65175
Guillaume Raimondeau <raimondeau@gmail.com>
2016-07-14 22:05:45 +0200

    some files to test

--- Go/book/dup2.go
+++ Go/book/dup2.go
@@ -1,5 +1,6 @@
 //dup2 prints the count and text of lines that appear more than once
 //in the input. It reads from stdin or from a list of named files.
+//go run dup2.go dup2.txt dup2_bis.txt 
 package main
 
 import(
@@ -14,16 +15,18 @@ func main() {
     if len(files)==0 {
         countLines(os.Stdin, counts)
     } else {
-        for _,arg := files {
+        for k,arg := range files {
             f, err := os.Open(arg)
         if err != nil {
             fmt.Fprintf(os.Stderr,"dup2:%v\n",err)
             continue
             }
+        fmt.Println(files[k])
         countLines(f, counts)
         f.Close()
         }
     }
+
     for line,n := range counts {
         if n > 1 {
             fmt.Printf("%d\t%s\n",n,line)
