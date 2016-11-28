f9526473cc25f2da90cc23b6f32ee56636d453e0
Guillaume Raimondeau <raimondeau@gmail.com>
2016-11-28 18:17:30 +0100

    commit

--- Go/fb/main.go
+++ Go/fb/main.go
@@ -9,69 +9,71 @@ const (
     THROW ="THROW"
     )
 type command struct{
-    ctype string,
-    destination position,
-    power int,
+    ctype string
+    destination position
+    power int
 }
 
 func main() {
+    const (
+		HEIGHT=7501
+		WIDTH=16001
+    )
     // myTeamId: if 0 you need to score on the right of the map, if 1 you need to score on the left
     var myTeamId int
     fmt.Scan(&myTeamId)
-    
-    const (
-        goalTeamO=NewPosition(0,3750)
-	goalTeam1=NewPosition(16000,3750)
-	HEIGHT=7501
-	WIDTH=16001
-        )
+	var myGoal Position
+	switch myTeamId{
+		case 0:
+			 myGoal=NewPosition(0,3750)
+		case 1:
+			 myGoal=NewPosition(16000,3750)
+	}
         
     for {
         // entities: number of entities still in game
         var entities int
         fmt.Scan(&entities)
-	var myWiz []Entity
-	var snaffle []Entity
-	var oppWiz []Entity
+		var myWiz []Entity
+		var snaffle []Entity
+		var oppWiz []Entity
         for i := 0; i < entities; i++ {
-            // entityId: entity identifier
             // entityType: "WIZARD", "OPPONENT_WIZARD" or "SNAFFLE" (or "BLUDGER" after first league)
-            // x: position
-            // y: position
-            // vx: velocity
-            // vy: velocity
             // state: 1 if the wizard is holding a Snaffle, 0 otherwise
             var entityId int
             var entityType string
             var x, y, vx, vy, state int
-		fmt.Scan(&entityId, &entityType, &x, &y, &vx, &vy, &state)
-		if entityType=="WIZARD"{
-			myWiz=append(myWiz,newEntity(entityId, entityType, x, y, vx, vy, state))
-		} else if entityType=="OPPONENT_WIZARD"{
-			oppWiz=append(oppWiz,newEntity(entityId, entityType, x, y, vx, vy, state))
-		} else if entityType=="SNAFFLE"{
-			snaffle=append(snaffle,newEntity(entityId, entityType, x, y, vx, vy, state))
-		}
+			fmt.Scan(&entityId, &entityType, &x, &y, &vx, &vy, &state)
+			if entityType=="WIZARD"{
+				myWiz=append(myWiz,newEntity(entityId, entityType, x, y, vx, vy, state))
+			} else if entityType=="OPPONENT_WIZARD"{
+				oppWiz=append(oppWiz,newEntity(entityId, entityType, x, y, vx, vy, state))
+			} else if entityType=="SNAFFLE"{
+				snaffle=append(snaffle,newEntity(entityId, entityType, x, y, vx, vy, state))
+			}
+	}
 				
-        }
-	    //check for the closest snaffle?
-	    func pickSnaffle(wizard Entity,snaffles []Entity) Entity{
-		    var best=WIDTH
-		    for _,snaffle :=range snaffles{
-			    distance:=distEntity(wizard,snaffle)
-			    if distance < best{
-				    best=distance
-			    }
-		    }
-	    }
+	//check for the closest snaffle?
+	func pickSnaffle(wizard Entity,snaffles []Entity) Entity{
+	    var best=WIDTH
+		var closestSnaffle Entity
+	    for _,snaffle :=range snaffles{
+	        distance:=distEntity(wizard,snaffle)
+	        if distance < best{
+				best=distance
+				closestSnaffle=snaffle
+			}
+		}
+		return closestSnaffle
+	}
 			    
-        for i := 0; i < 2; i++ {
+        //for i := 0; i < 2; i++ {
             
             // fmt.Fprintln(os.Stderr, "Debug messages...")
             
             // Edit this line to indicate the action for each wizard (0 <= thrust <= 150, 0 <= power <= 500)
             // i.e.: "MOVE x y thrust" or "THROW x y power"
-            fmt.Printf("MOVE 8000 3750 100\n")
-        }
+            //fmt.Printf("MOVE 8000 3750 100\n")
+        //}
     }
 }
---
+++ _emacs
@@ -0,0 +1,37 @@
+;;no scroll bar
+(scroll-bar-mode -1)
+
+;;line numbers//does not work!!
+;;(linum-mode 1)
+
+;;Elpa melpa..
+(package-initialize)
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
+
+;;GOLANG
+(defun go-mode-setup ()
+  (go-eldoc-setup))
+(add-hook 'go-mode-hook 'go-mode-setup)
+
+;;Format before saving
+(defun go-mode-setup ()
+  (go-eldoc-setup)
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (setq tab-width 4)
+  (setq indent-tabs-mode 1))
+(add-hook 'go-mode-hook 'go-mode-setup)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (go-eldoc auto-complete go-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
