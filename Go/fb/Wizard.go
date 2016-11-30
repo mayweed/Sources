eddbd76664012e076ceca16abc1cd30e45a0204b
Guillaume Raimondeau <raimondeau@gmail.com>
2016-11-30 11:14:39 +0100

    New forgotten file

---
+++ Go/fb/Wizard.go
@@ -0,0 +1,33 @@
+package fb
+
+//should use position here
+type Wizard struct {
+	entityId int
+	entityType string
+	x          int
+	y          int
+	vx         int
+	vy         int
+	state      int
+}
+
+func newWizard(id int, etype string, x, y, vx, vy, state int) Wizard {
+	if etype!="SNAFFLE"{
+		return Entity{
+			entityId:   id,
+			entityType: etype,
+			x:          x,
+			y:          y,
+			vx:         vx,
+			vy:         vy,
+			state:      state,
+		}
+}
+
+func (w Wizard) hasGrabbedSnaffle() bool {
+	if w.state == 1 {
+		return true
+	} else {
+		return false
+	}
+}
