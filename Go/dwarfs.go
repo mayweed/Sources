a398bd0afe006e1a516fb4013ccb93b059f25ff5
Guillaume Raimondeau <raimondeau@gmail.com>
2017-01-27 20:54:59 +0100

    bfs work level not

--- Go/dwarfs.go
+++ Go/dwarfs.go
@@ -15,6 +15,7 @@ func newGraph() graph {
 	return graph{
 		nodes: nil,
 		//edges: make(map[int][]int),
+        /*  depth max 6
 		edges: map[int][]int{
 			5: []int{3, 6},
 			6: []int{1, 2},
@@ -23,6 +24,15 @@ func newGraph() graph {
 			4: []int{5},
 			2: []int{8},
 		},
+        */
+        //depth max 4
+        edges: map[int][]int{
+			10: []int{1, 3, 11},
+			1: []int{2, 3},
+			3: []int{4},
+			2: []int{4,5},
+		},
+
 		//TODO find a way to quickly parse that
 		//oki string: first split on space then split num on ""
 		//or num[0] => x num[1] =>y
@@ -50,28 +60,50 @@ func (g graph) dfs(startNode int) int {
 	return height
 }
 
-func (g graph) bfs(startNode int) {
+//very primitive should write that with 2 queues one 
+//for the current and one for the next
+// first comm http://stackoverflow.com/questions/10258305/how-to-implement-a-breadth-first-search-to-a-certain-depth/16923440#16923440
+func (g graph) bfs(startNode int) int{
 	var visited = make(map[int]bool)
 	visited[startNode] = true
 
 	var queue []int
 	queue = append(queue, startNode)
 
+    var depth int
+    var pendingDepthIncrease bool
 
 	for 0 < len(queue) {
+        var timeToDepthIncrease=len(queue)
+        log.Println("INIT",timeToDepthIncrease)
         //pop the first element
 		v := queue[0]
         queue=queue[1:]
+
+        timeToDepthIncrease-=1
+        log.Println("END",timeToDepthIncrease)
+
 
 		for _,w := range g.edges[v] {
 			if !visited[w] {
 				visited[w] = true
 				queue = append(queue, w)
-				log.Println(queue,visited)
-			}
-		}
+                if pendingDepthIncrease{
+                    timeToDepthIncrease=len(queue)-1
+                    pendingDepthIncrease=false
+                    }
+				//log.Println(queue,visited)
+            }
+        }
+        if timeToDepthIncrease==0{
+                depth+=1
+                pendingDepthIncrease=true
+        }
+
+		//}
 
 	}
+    return depth
 }
 
 func main() {
@@ -96,18 +128,17 @@ func main() {
 	// fmt.Fprintln(os.Stderr, "Debug messages...")
 
 	// The number of people involved in the longest succession of influences
-	//var max = 0
-	var height int
+	var max = 0
+	//var height int
 	for k, _ := range g.edges {
-		log.Println("Edges of node", k)
 		//height := g.dfs(k)
-		g.bfs(k)
+		height:=g.bfs(k)
 
-		//if height > max {
-		//	max = height
-		//}
+		if height > max {
+			max = height
+		}
 
-		//fmt.Printf("Node %d has depth %d\n", k, height)
+		fmt.Printf("Node %d has depth %d\n", k, height)
 	}
-	fmt.Println(height)
+	fmt.Println(max)
 }
