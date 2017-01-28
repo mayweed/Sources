cd8960495b8ff8fd52cbde209087ef2467bb8f4b
Guillaume Raimondeau <raimondeau@gmail.com>
2017-01-28 09:45:13 +0100

    backup

---
+++ Go/timer.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	//"log"
+	"math"
+)
+
+func tsToSec(ts string) int {
+	var seconds int
+	var min int
+
+	ss := strings.Split(ts, ":")
+	min, _ = strconv.Atoi(ss[0])
+	min *= 60
+
+	seconds, _ = strconv.Atoi(ss[1])
+	seconds += min
+
+	return seconds
+}
+
+func calculusToTs(sec int) string {
+	var min int
+	for sec > 60 {
+		sec -= 60
+		min++
+	}
+	ts := fmt.Sprintf("%d:%d", min, sec)
+	return ts
+}
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Buffer(make([]byte, 1000000), 1000000)
+
+	var n int
+	scanner.Scan()
+	fmt.Sscan(scanner.Text(), &n)
+
+	var calculus float64
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		timeStamp := scanner.Text()
+
+		t := tsToSec(timeStamp)
+		calculus = float64(t) - 256./(math.Exp2(float64(n-1)))
+		//seconds:=float64(t)-calculus
+
+		//log.Println(timeStamp,tsToSec(timeStamp),calculusToTs(int(calculus)),seconds)
+		//fmt.Printf("It starts %d seconds from now\n",int(seconds))
+	}
+
+	var s string
+	if calculus < 0 {
+		s = "0:00"
+	} else {
+		s = calculusToTs(int(calculus))
+	}
+	if n == 0 {
+		s = "NO GAME"
+	}
+	fmt.Println(s) // Write answer to stdout
+}
