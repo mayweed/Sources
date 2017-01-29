ea9accca68467978af5dfd24bc23fe5617e34d41
Guillaume Raimondeau <raimondeau@gmail.com>
2017-01-29 15:56:38 +0100

    cant pass coz it displays 0:9 instead of 0:09 when timetostart < 10sec!!

--- Go/timer.go
+++ Go/timer.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"bufio"
+//	"bufio"
 	"fmt"
-	"log"
-	"math"
-	"os"
+//	"log"
+	//"math"
+	//"os"
 	"strconv"
 	"strings"
 )
@@ -30,7 +30,12 @@ func secToTs(sec int) string {
 		sec -= 60
 		min++
 	}
-	ts := fmt.Sprintf("%d:%d", min, sec)
+    var ts string
+    if sec < 10{
+        ts=fmt.Sprintf("%d:0%d",min,sec)
+    }else{
+	    ts= fmt.Sprintf("%d:%d", min, sec)
+    }
 	return ts
 }
 
