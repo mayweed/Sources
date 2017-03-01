f54f44870d9af15ec2b1378dcbbf41863bb3b455
Guillaume Raimondeau <raimondeau@gmail.com>
2017-03-01 11:59:34 +0100

    mmm...

--- Go/gitc.go
+++ Go/gitc.go
@@ -9,17 +9,25 @@ type graph struct {
 	factoryCount int
 	linkCount    int
 	//a int from a slice of ints(factory2 + distance)
-	edges map[int][][]int
-
+	edges     map[int][][]int
 	factories []factory
 	troops    []troop
 }
+
 type factory struct {
 	id         int
 	cyborgs    int
 	production int
-	//arg1 to know
-	owner int
+	owner      int
+}
+
+//helper func
+func amIowner(f factory) bool {
+	if f.owner == 1 {
+		return true
+	} else {
+		return false
+	}
 }
 
 type troop struct {
@@ -28,6 +36,25 @@ type troop struct {
 	cyborgs        int
 	remainingTurns int
 	owner          int
+}
+
+//GRAPH METHODS
+func (g graph) getFactory(id int) factory {
+	for _, fac := range g.factories {
+		if fac.id == id {
+			return fac
+		}
+	}
+	return factory{}
+}
+func (g graph) getFactQueue(startNode factory) []factory {
+	var queue []factory
+	for k, _ := range g.edges {
+		if k != startNode.id {
+			queue = append(queue, g.getFactory(k))
+		}
+	}
+	return queue
 }
 
 //oki does not work
@@ -45,21 +72,26 @@ func (g graph) countScore() (x, y int) {
 	return myScore, oppScore
 }
 
-func (g graph) pickAnotherFactory(queue, doneQueue []int, num int) (q1, q2 []int, node factory) {
+func (g graph) pickAnotherFactory(queue []factory, doneQueue []int, num int, lastStart factory) (q1 []factory, q2 []int, node factory) {
 	//oki so: take factory from a done queue check cyborgs num
 	//and promote it to start node
 	var startNode factory
+	//should select the one with maxnodes?
+	var min = num * 2
 	for _, factory := range g.factories {
-		if factory.id == doneQueue[0] {
-			//num == number of sent cyborgs
-			if factory.cyborgs >= num && factory.owner == 1 {
-				startNode = factory
+		//should own the factory
+		if factory.owner == 1 {
+			if factory.id == doneQueue[0] {
+				//num == number of sent cyborgs
+				if factory.cyborgs >= min && factory.owner == 1 {
+					startNode = factory
+				}
 			}
 		}
 	}
 	//in that case should clean queue of the new start node
 	for _, v := range queue {
-		if v == startNode.id {
+		if v.id == startNode.id {
 			continue
 		} else {
 			queue = append(queue, v)
@@ -69,22 +101,27 @@ func (g graph) pickAnotherFactory(queue, doneQueue []int, num int) (q1, q2 []int
 }
 
 //return the id of the nearest node of a given factory
+//and a factory I own if possible
 //BETTER:order those slices in an id slice with nearest first
 func (g graph) pickMinNode(f factory) int {
 	var minDist = 20
 	var id int
 	var idSlice []int
+	fact := g.getFactory(f.id)
 	for _, v := range g.edges[f.id] {
-		if v[1] < minDist {
-			minDist = v[1]
-			id = v[0]
-			idSlice = append(idSlice, id)
+		if fact.owner == 1 {
+			if v[1] < minDist {
+				minDist = v[1]
+				id = v[0]
+				idSlice = append(idSlice, id)
+			}
 		}
 		//log.Println(idSlice)
 	}
 	return id
 }
 
+//COMMAND HELPER
 func mv(from, to, cyb int) string {
 	s := fmt.Sprintf("MOVE %d %d %d\n", from, to, cyb)
 	return s
@@ -115,9 +152,8 @@ func main() {
 
 	//enqueue nodes
 	//so two queues : one with dest factory and the second one with send
-	var queue []int
+	var queue []factory
 	var sendQueue []int
-	var myFact []factory
 
 	for {
 		// entityCount: the number of entities (e.g. factories and troops)
@@ -150,40 +186,49 @@ func main() {
 				network.troops = append(network.troops, troop{arg2, arg3, arg4, arg5, arg1})
 			}
 		}
-		myFact = myFactories
-		var startNode = myFact[0]
-		log.Println("STARTNODE FIRST", startNode.id, "NUM", startNode.cyborgs)
-		myFact = myFact[1:]
 
-		//DOES NOT WORK AS INTENDED especially:
-		var s string
-		var num = 3
+		//BFS like non? should i use myFactories or queue?
+		for len(myFactories) > 0 {
+			var startNode = myFactories[0]
+			//log.Println("STARTNODE FIRST",startNode.id,"NUM",startNode.cyborgs)
+			log.Println(myFactories, oppFactories)
 
-		for k, _ := range network.edges {
-			if k != startNode.id {
-				queue = append(queue, k)
+			var s string
+			var num = 3
+
+			queue = network.getFactQueue(startNode)
+
+			//WHY in the hell use an int here?
+			dest := queue[0].id
+			//c trop nul ça...tss
+			//if startNode.id==dest{dest=queue[1].id}
+			//should clean queue instead...
+			if startNode.cyborgs > num {
+				if startNode.id == dest {
+					dest = queue[1].id
+				}
+				s = mv(startNode.id, dest, num)
+				sendQueue = append(sendQueue, dest)
+			} else if startNode.cyborgs <= num {
+				//the other way round: bfs? neighboring nodes of startNode?
+				queue, sendQueue, startNode = network.pickAnotherFactory(queue, sendQueue, num, startNode)
+				if startNode.id == dest {
+					dest = network.pickMinNode(startNode)
+				}
+				s = mv(startNode.id, dest, num)
+				log.Println("I was here", startNode.id)
 			}
+			//oki new errors:
+			//Can't send a troop to the factory it is issued from (0)
+			//OOPS!! src and dest MUST be different!!
+			fmt.Printf("%s", s)
+			//log.Println(network.countScore())
+			//put nodes at end
+			//queue=append(queue[1:],dest)
+			//WHY TWO QUEUES??
+			queue = queue[1:]
+			myFactories = myFactories[1:]
 		}
-
-		dest := queue[0]
-		if startNode.cyborgs > num {
-			s = mv(startNode.id, dest, num)
-			sendQueue = append(sendQueue, dest)
-		} else if startNode.cyborgs <= num {
-			//the other way round: bfs? neighboring nodes of startNode?
-			queue, sendQueue, startNode = network.pickAnotherFactory(queue, sendQueue, num)
-			s = mv(startNode.id, dest, num)
-			log.Println("I was here", startNode.id)
-
-		}
-
-		//oki new errors:
-		//Can't send a troop to the factory it is issued from (0)
-		//log.Println(min,lastSendTroopsNode,neutralFactories[0].id,network.edges)
-		// Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
-		fmt.Printf("%s", s)
-		log.Println(network.countScore())
-		//put nodes at end
-		queue = append(queue[1:], dest)
+		//fmt.Printf("WAIT\n")
 	}
 }
