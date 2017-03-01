2369b715063f41d1d870e8ffcec27e712a167d90
Guillaume Raimondeau <raimondeau@gmail.com>
2017-03-01 09:30:04 +0100

    oki it sends to multiple nodes, I can rebase but always got the same error about issuing illegaly troops

--- Go/gitc.go
+++ Go/gitc.go
@@ -8,7 +8,7 @@ import (
 type graph struct {
 	factoryCount int
 	linkCount    int
-	//a slice of int slices(dest + distance)
+	//a int from a slice of ints(factory2 + distance)
 	edges map[int][][]int
 
 	factories []factory
@@ -19,7 +19,7 @@ type factory struct {
 	cyborgs    int
 	production int
 	//arg1 to know
-	ownership int
+	owner int
 }
 
 type troop struct {
@@ -27,7 +27,62 @@ type troop struct {
 	to             int
 	cyborgs        int
 	remainingTurns int
-	ownership      int
+	owner          int
+}
+
+//oki does not work
+func (g graph) countScore() (x, y int) {
+	var myScore = 0
+	var oppScore = 0
+	for _, v := range g.factories {
+		switch v.owner {
+		case 1:
+			myScore += v.cyborgs
+		case -1:
+			oppScore += v.cyborgs
+		}
+	}
+	return myScore, oppScore
+}
+
+func (g graph) pickAnotherFactory(queue, doneQueue []int, num int) (q1, q2 []int, node factory) {
+	//oki so: take factory from a done queue check cyborgs num
+	//and promote it to start node
+	var startNode factory
+	for _, factory := range g.factories {
+		if factory.id == doneQueue[0] {
+			//num == number of sent cyborgs
+			if factory.cyborgs >= num && factory.owner == 1 {
+				startNode = factory
+			}
+		}
+	}
+	//in that case should clean queue of the new start node
+	for _, v := range queue {
+		if v == startNode.id {
+			continue
+		} else {
+			queue = append(queue, v)
+		}
+	}
+	return queue, doneQueue, startNode
+}
+
+//return the id of the nearest node of a given factory
+//BETTER:order those slices in an id slice with nearest first
+func (g graph) pickMinNode(f factory) int {
+	var minDist = 20
+	var id int
+	var idSlice []int
+	for _, v := range g.edges[f.id] {
+		if v[1] < minDist {
+			minDist = v[1]
+			id = v[0]
+			idSlice = append(idSlice, id)
+		}
+		//log.Println(idSlice)
+	}
+	return id
 }
 
 func mv(from, to, cyb int) string {
@@ -49,14 +104,21 @@ func main() {
 		linkCount:    linkCount,
 		edges:        make(map[int][][]int),
 	}
-
 	for i := 0; i < linkCount; i++ {
 		var factory1, factory2, distance int
 		fmt.Scan(&factory1, &factory2, &distance)
-		//not directed, should i use a edge struct?
+		//not directed SHOULD BE al list of lists... to handle multiple edges
+		//or list of edges
 		network.edges[factory1] = append(network.edges[factory1], []int{factory2, distance})
 		network.edges[factory2] = append(network.edges[factory2], []int{factory1, distance})
 	}
+
+	//enqueue nodes
+	//so two queues : one with dest factory and the second one with send
+	var queue []int
+	var sendQueue []int
+	var myFact []factory
+
 	for {
 		// entityCount: the number of entities (e.g. factories and troops)
 		var entityCount int
@@ -88,17 +150,15 @@ func main() {
 				network.troops = append(network.troops, troop{arg2, arg3, arg4, arg5, arg1})
 			}
 		}
-		var startNode = myFactories[0]
-		myFactories = myFactories[1:]
+		myFact = myFactories
+		var startNode = myFact[0]
+		log.Println("STARTNODE FIRST", startNode.id, "NUM", startNode.cyborgs)
+		myFact = myFact[1:]
 
 		//DOES NOT WORK AS INTENDED especially:
-		//IDEA: take all v[0] of my startNode put theme in a queue
-		//dequeue and send packets...
-		//var min =network.pickMinNode(startNode)
 		var s string
 		var num = 3
 
-		//TODO factorize plz
 		for k, _ := range network.edges {
 			if k != startNode.id {
 				queue = append(queue, k)
@@ -106,26 +166,23 @@ func main() {
 		}
 
 		dest := queue[0]
-		//oki new errors:
-		//Can't send a troop to the factory it is issued from (0)
-
 		if startNode.cyborgs > num {
 			s = mv(startNode.id, dest, num)
-		} else {
-			//pick another starting node
-			//should check that it has cyborgs?
+			sendQueue = append(sendQueue, dest)
+		} else if startNode.cyborgs <= num {
 			//the other way round: bfs? neighboring nodes of startNode?
-			//check that the nearest node has cyborgs and use it as new base?
-			//SHOULD BE ABLE TO REBASE
-			//SHOULD KEEP TRACK OF CYBIORGS NUM with production rate. Should not send
-			//more cyborgs than produce?
-			startNode = queue[len(queue)]
+			queue, sendQueue, startNode = network.pickAnotherFactory(queue, sendQueue, num)
+			s = mv(startNode.id, dest, num)
+			log.Println("I was here", startNode.id)
+
 		}
+
+		//oki new errors:
+		//Can't send a troop to the factory it is issued from (0)
 		//log.Println(min,lastSendTroopsNode,neutralFactories[0].id,network.edges)
 		// Any valid action, such as "WAIT" or "MOVE source destination cyborgs"
-		//fmt.Sprintf("MOVE" startNode network.edges[startNode][0] prod)
 		fmt.Printf("%s", s)
-
+		log.Println(network.countScore())
 		//put nodes at end
 		queue = append(queue[1:], dest)
 	}
