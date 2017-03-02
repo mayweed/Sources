699ba64c06d6efc822c8894cd15cc8af9787eebf
Guillaume Raimondeau <raimondeau@gmail.com>
2017-03-02 15:50:17 +0100

    backup

--- Go/gitc.go
+++ Go/gitc.go
@@ -34,9 +34,8 @@ func amIowner(f factory) bool {
 //slice thing does not work
 func (g graph) maxProdFactory(f []factory) (factory, []factory) {
 	var maxFact factory
+	//cf go blog slice
 	//newSlice := make([]int, len(slice), 2*cap(slice))
-	//we suppose it wont be longer than all the factories but...
-	//should play with len and cap to have "fit" list
 	var maxFacts = make([]factory, len(g.factories))
 	var max = 0
 	for _, fact := range f {
@@ -66,17 +65,19 @@ type troop struct {
 
 //OKI this is a test never done that before
 //IDEA: develop a game state + a tree of possible moves
-//from where I am where I could go first and then calculate
-//best move
 type gameState struct {
+	//our sources
 	myCurrentBase factory
 	oppBase       factory
-
-	//Troops
-	myTroops  int
-	oppTroops int
+	//our destinations
+	myLastDest  factory
+	oppLastDest factory
+	//our troops
+	myTroops  []troop
+	oppTroops []troop
 
 	numOfTurns int
+	score      int
 
 	possibleMoves []move
 }
@@ -96,12 +97,6 @@ func (g graph) getFactory(id int) factory {
 	return factory{}
 }
 
-//QUESTION:what do I want in that queue?
-// ANSWER: I use it to pick my destination
-//func (g graph) pickAFactory(facts []factory) factory{
-//Critères: d'abord les - éloignés plus productives
-//for _,v :=range facts{
-
 func (g graph) getFactQueue(startNode factory) []factory {
 	var queue []factory
 	for k, _ := range g.edges {
@@ -112,39 +107,27 @@ func (g graph) getFactQueue(startNode factory) []factory {
 	return queue
 }
 
-func (g graph) pickAnotherFactory(queue []factory, doneQueue []int, num int, lastStart factory) (q1 []factory, q2 []int, node factory) {
-	//oki so: take factory from a done queue check cyborgs num
-	//and promote it to start node
-	//I should ensure that the startNode yielded is not already one!!
+func (g graph) pickSourceFactory(queue []factory, num int, lastStart factory) (node factory) {
 	var startNode factory
 	//should select the one with maxnodes?
 	for _, factory := range g.factories {
 		//should own the factory
 		if factory.owner == 1 {
-			//POURQUOI done queue?
-			if factory.id == doneQueue[0] {
-				//num == number of sent cyborgs
-				if factory.cyborgs >= num && factory.owner == 1 && factory.id != lastStart.id {
-					startNode = factory
-				}
+			if factory.cyborgs >= num && factory.id != lastStart.id {
+				startNode = factory
 			}
 		}
 	}
-	//in that case should clean queue of the new start node
-	for _, v := range queue {
-		if v.id == startNode.id {
-			continue
-		} else {
-			queue = append(queue, v)
-		}
-	}
-	return queue, doneQueue, startNode
+	return startNode
 }
+
+//func (g graph) pickDestFactory(facts []factory,startNode factory) factory{
+//First minNode + maxProd of opponent?
+//for _,v := range (g.factories){
 
 //return the id of the nearest node of a given factory
 //and a factory I own if possible
-//BETTER:order those slices in an id slice with nearest first
-func (g graph) pickMinNode(f factory) int {
+func (g graph) pickMinNode(f factory) factory {
 	var minDist = 20
 	var id int
 	var idSlice []int
@@ -157,9 +140,9 @@ func (g graph) pickMinNode(f factory) int {
 				idSlice = append(idSlice, id)
 			}
 		}
-		//log.Println(idSlice)
 	}
-	return id
+	f := g.getFactory(id)
+	return f
 }
 
 //SCORE
@@ -209,7 +192,7 @@ func main() {
 		linkCount:    linkCount,
 		edges:        make(map[int][][]int),
 	}
-	//eval:=gameState{}
+	eval := gameState{}
 
 	for i := 0; i < linkCount; i++ {
 		var factory1, factory2, distance int
@@ -223,8 +206,6 @@ func main() {
 	//enqueue nodes
 	//so two queues : one with dest factory and the second one with send
 	var queue []factory
-	var sendQueue []int
-	var turns int
 	var myScore int
 	var oppScore int
 
@@ -271,15 +252,11 @@ func main() {
 		//BFS like non? should i use myFactories or queue?
 		//Think: myFactories those are my base start. Should attack oppFactories
 		for len(myFactories) > 0 {
-			var startNode = myFactories[0]
-			log.Println("EDGES STARTNODE:", len(network.edges[startNode.id]))
-			//eval.myCurrentBase=startNode
-			//log.Println("STARTNODE FIRST",startNode.id,"NUM",startNode.cyborgs)
-			log.Println(myFactories, oppFactories)
-
 			var s string
 			var num = 3
-
+			var startNode = network.pickSourceFactory(myFactories, num, factory{})
+			eval.myCurrentBase = startNode
+			//log.Println(myFactories,oppFactories)
 			queue = network.getFactQueue(startNode)
 			log.Println(queue)
 
@@ -287,19 +264,11 @@ func main() {
 			//HERE I should pick either a neutral or an opponent fact!!
 			dest := queue[0].id
 
-			//c trop nul ça...tss
-			//if startNode.id==dest{dest=queue[1].id}
-			//should clean queue instead...
-			if startNode.cyborgs > num {
-				//if startNode.id==dest{
-				//    dest=queue[1].id
-				//}
+			///THIS IS checked in pick source??
+			if startNode.cyborgs >= num {
 				s = mv(startNode.id, dest, num)
-				sendQueue = append(sendQueue, dest)
-			} else if startNode.cyborgs <= num {
-				//the other way round: bfs? neighboring nodes of startNode?
-				queue, sendQueue, startNode = network.pickAnotherFactory(queue, sendQueue, num, startNode)
-				//if startNode.id==dest{dest=network.pickMinNode(startNode)}
+			} else if startNode.cyborgs < num {
+				startNode = network.pickSourceFactory(myFactories, num, startNode)
 				s = mv(startNode.id, dest, num)
 				log.Println("I was here", startNode.id)
 			}
@@ -313,14 +282,12 @@ func main() {
 			queue = queue[1:]
 			myFactories = myFactories[1:]
 		}
-		//fmt.Printf("WAIT\n")
+		eval.numOfTurns += 1
 
-		turns += 1
-		log.Println(turns)
-		//myScore,oppScore=
+		//SCORE, does not work...
 		network.countTroops(&Score, &opp)
-		log.Println(Score, opp)
-		log.Println(myScore, oppScore)
+		//log.Println(Score,opp)
+		//log.Println(myScore,oppScore)
 
 	}
 	//should be reset at the end of each turn
