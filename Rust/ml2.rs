3dd11d94f6ebe790ce81b82a67068822692a254a
Guillaume Raimondeau <raimondeau@gmail.com>
2018-01-13 18:01:26 +0100

    playing harcode to get the physics of hspeed vspeed etc...

--- Rust/ml2.rs
+++ Rust/ml2.rs
@@ -2,6 +2,7 @@
 //objective here: simulate mars landing and use monte carlo or gen algo
 //https://pastebin.com/3SUUjgSF
 use std::io;
+use std::f64::consts::PI;
 
 macro_rules! print_err {
     ($($arg:tt)*) => (
@@ -26,6 +27,9 @@ const LANDED:i32 = 1;
 const LAND_BAD_ANGLE:i32 = 2;
 const LAND_BAD_SPEED:i32 = 3;
 const CRASH:i32 = 4;
+
+//TEST
+const DEPTH:i32 = 5;
 
 //POINT should I use <T> here??
 #[derive(Debug,Copy,Clone)]
@@ -71,7 +75,7 @@ impl flat_ground{
             }
     }
     
-    //return a bool
+    //return a bool:will next pos of lander collides with surface?
     fn collide(self,landerx0:f64,landery0:f64,landerx1:f64,landery1:f64) -> bool{
         let mut s2_x = landerx1 - landerx0;
         let mut s2_y = landery1 - landery0;
@@ -110,35 +114,86 @@ impl flat_ground{
         print_err!("{:?}",landing_zone);
         //cant get that type of thing to work in rust!!
         //mismatched types: expected (); found reference
-        //let landing_zone= for line in landlines.iter(){
+        //diff iter() into_iter() ter(), which iterates over &T.
+        //iter_mut(), which iterates over &mut T.
+        //into_iter(), which iterates over T.
+        //let for line in landlines.into_iter(){
         //    if line.is_landing_zone(){
         //        line
         //        }
-        //     };
+        //     } = landing_zone;
         landing_zone
     }
 }
 
+//TRIGO => macros??
+fn toRadians (angle:f64) -> f64{
+  angle * (PI / 180.0)
+}
+ 
+fn sinDeg(deg:f64) -> f64{
+  f64::sin(toRadians(deg))
+}
+ 
+fn cosDeg (deg:f64) -> f64{
+  f64::cos(toRadians(deg))
+}
+
+//GAMESTATE
+struct game_state{
+    turn:i32,
+    last_position:Point, //for oldx/oldy
+    curr_position:Point,
+    vx:f64,
+    vy:f64,
+    ax:f64,
+    ay:f64,
+    fuel:f64,
+    power:f64,
+    }
+    
+struct Genoma{
+    rotation:Vec<f64>,
+    power:Vec<f64>,
+    score:f64,
+}
 fn main() {
     //first find a landing spot
     let mut landing_ground=flat_ground::find_landing_zone();
+    let mut turn=1;
+    let mut prev_x=0.0;
+    let  mut prev_y=0.0;
     
     // game loop
     loop {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let inputs = input_line.split(" ").collect::<Vec<_>>();
-        let x = parse_input!(inputs[0], i32);
-        let y = parse_input!(inputs[1], i32);
-        let h_speed = parse_input!(inputs[2], i32); // the horizontal speed (in m/s), can be negative.
-        let v_speed = parse_input!(inputs[3], i32); // the vertical speed (in m/s), can be negative.
-        let fuel = parse_input!(inputs[4], i32); // the quantity of remaining fuel in liters.
+        let x = parse_input!(inputs[0], f64);
+        let y = parse_input!(inputs[1], f64);
+        let h_speed = parse_input!(inputs[2], f64); // the horizontal speed (in m/s), can be negative.
+        let v_speed = parse_input!(inputs[3], f64); // the vertical speed (in m/s), can be negative.
+        let fuel = parse_input!(inputs[4], f64); // the quantity of remaining fuel in liters.
         let rotate = parse_input!(inputs[5], i32); // the rotation angle in degrees (-90 to 90).
         let power = parse_input!(inputs[6], i32); // the thrust power (0 to 4).
         
-        print_err!("hspeed:{}, vspeed:{}, fuel:{}, rotate:{}, power:{}",h_speed,v_speed,fuel,rotate,power);
-        
+        let mut position:Point=Point{x:x, y:y };
+        turn+=1;
+        print_err!("hspeed:{}, vspeed:{}, fuel:{}, rotate:{}, power:{},turn:{},x:{},prev_x:{}",h_speed,v_speed,fuel,rotate,power,turn,x,prev_x);
+        prev_x=x;
+        prev_y=y;
         // rotate power. rotate is the desired rotation angle. power is the desired thrust power.
-        println!("0 2");
+       
+        if position.distance(&landing_ground.point0) <=1500.0{
+            println!("0 4");
+        }else{
+            //if v_speed > -50.0{
+                println!("-20 3");
+               // }else{
+               // println!("-20 4");
+               // }
+            }
+        print_err!("{}",position.distance(&landing_ground.point0));
     }
+    
 }
