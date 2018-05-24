894a8f9968c8460ff90248c7cf3ec9d0b065377f
Guillaume Raimondeau <raimondeau@gmail.com>
2018-05-24 16:58:25 +0200

    rm old files

--- Bash/CGoban.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# prefere l'alias...
-
-/usr/bin/javaws /home/guillaume/cgoban-nfa.jnlp
-
-
-#export JAVAWS_HOME=/usr/bin/javaws
-#wget "http://files.gokgs.com/javaBin/cgoban.jnlp"
-#javaws cgoban.jnlp &
