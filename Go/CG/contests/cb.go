105ca34125ec00db54a68353ee2da341e48bf1dc
Guillaume Raimondeau <raimondeau@gmail.com>
2018-09-12 09:48:08 +0200

    cleaning some old code thinking about entering the game

--- Go/CG/contests/cb.go
+++ Go/CG/contests/cb.go
@@ -1,58 +1,83 @@
 package main
 
-import "fmt"
-import "math"
-import "math/rand"
-import "os"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+)
 
-// POSITION
-type position struct {
+type Point struct {
 	x, y int
 }
 
-func NewPosition(x, y int) position {
-	return position{
+func setPoint(x, y int) Point {
+	return Point{
 		x: x,
 		y: y,
 	}
 }
-func (p position) Move(x, y int) (int, int) {
+func (p Point) Move(x, y int) Point {
 	x = p.x + x
 	y = p.y + y
-	return x, y
-}
-func (p position) distFrom(x, y int) float64 {
-	dist := math.Sqrt((float64(p.x)-float64(x))*(float64(p.x)-float64(x)) + (float64(p.y)-float64(y))*(float64(p.y)-float64(y)))
-	return dist
-}
-func HomePosition(myTeamId int) position {
-	var home position
-	switch myTeamId {
-	case 0:
-		home = NewPosition(0, 0)
-	case 1:
-		home = NewPosition(16000, 9000)
-	}
-	return home
+	return Point{x, y}
 }
 
-//ENTITY
-type entity struct {
-	entityId, entityType, state, value int
-	pos                                position
+type State struct {
+	busters []Entity
+	ghosts  []Entity
+	//grab them by team id?
+	player []Player
+}
+type Player struct {
+	teamId         int
+	nbBusters      int
+	homeBase       Point
+	capturedGhosts []Entity
+}
+type Entity struct {
+	entityId   int
+	entityType int
+	state      int
+	value      int
+	pos        Point
 }
 
-func NewEntity(entityId, entityType, state, value int, pos position) entity {
-	return entity{
-		entityId:   entityId,
-		pos:        position{},
-		entityType: entityType,
-		state:      state,
-		value:      value,
+func readEntity() {
+	// entities: the number of busters and ghosts visible to you
+	var entities int
+	fmt.Scan(&entities)
+	var entityId, x, y, entityType, state, value int
+	//must be in a state
+	var ghosts_list []Entity
+	var my_busters_list []Entity
+	var foe_busters_list []Entity
+
+	for i := 0; i < entities; i++ {
+		// entityId: buster id or ghost id
+		// y: position of this buster / ghost
+		// entityType: the team id if it is a buster, -1 if it is a ghost.
+		// state: For busters: 0=idle, 1=carrying a ghost.
+		// value: For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
+
+		fmt.Scan(&entityId, &x, &y, &entityType, &state, &value)
+		if entityType == myTeamId {
+			b := Entity{entityId, entityType, state, value, Point{x, y}}
+			my_busters_list = append(my_busters_list, b)
+		}
+		if entityType == -1 {
+			ghosts_list = append(ghosts_list, Entity{entityId, entityType, state, value, Point{x, y}})
+		}
+		if entityType != myTeamId && entityType != -1 {
+			b := Entity{entityId, entityType, state, value, Point{x, y}}
+			foe_busters_list = append(foe_busters_list, b)
+		}
+
 	}
 }
 
 func main() {
+	//state.readParameters()
 	// bustersPerPlayer: the amount of busters you control
 	var bustersPerPlayer int
 	fmt.Scan(&bustersPerPlayer)
@@ -65,38 +90,20 @@ func main() {
 	var myTeamId int
 	fmt.Scan(&myTeamId)
 
+	//that switch should be in player init?
+	var home Point
+	switch myTeamId {
+	case 0:
+		home = Point{0, 0}
+	case 1:
+		home = Point{16000, 9000}
+	}
+
 	for {
-		// entities: the number of busters and ghosts visible to you
-		var entities int
-		fmt.Scan(&entities)
-		var entityId, x, y, entityType, state, value int
-		var ghosts_list []entity
-		var my_busters_list []entity
-		var foe_busters_list []entity
+		//state.readEntity()
 
-		for i := 0; i < entities; i++ {
-			// entityId: buster id or ghost id
-			// y: position of this buster / ghost
-			// entityType: the team id if it is a buster, -1 if it is a ghost.
-			// state: For busters: 0=idle, 1=carrying a ghost.
-			// value: For busters: Ghost id being carried. For ghosts: number of busters attempting to trap this ghost.
-
-			fmt.Scan(&entityId, &x, &y, &entityType, &state, &value)
-			if entityType == myTeamId {
-				b := NewEntity(entityId, entityType, state, value, NewPosition(x, y))
-				my_busters_list = append(my_busters_list, b)
-			}
-			if entityType == -1 {
-				ghosts_list = append(ghosts_list, NewEntity(entityId, entityType, state, value, NewPosition(x, y)))
-			}
-			if entityType != myTeamId && entityType != -1 {
-				b := NewEntity(entityId, entityType, state, value, NewPosition(x, y))
-				foe_busters_list = append(foe_busters_list, b)
-			}
-
-		}
+		//state.think()
 		//make them move to see the game
-		//if len(ghosts_list)!=0 should go and catch them!!
 		for i := 0; i < len(my_busters_list); i++ {
 			if u := len(ghosts_list); u != 0 {
 				//should iterate through ghosts_lists(pop?)
@@ -108,8 +115,7 @@ func main() {
 				fmt.Printf("MOVE %d %d\n", rand.Intn(16000), rand.Intn(9000))
 			}
 		}
-
-		fmt.Fprintln(os.Stderr, myTeamId, bustersPerPlayer, ghostCount, entities, len(my_busters_list), len(ghosts_list))
+		//state.act()
 		//fmt.Printf("MOVE 8000 4500\n") // MOVE x y | BUST id | RELEASE
 	}
 }
