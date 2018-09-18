b5e730228b20e11f14f11d507390257cff6dfd81
Guillaume Raimondeau <raimondeau@gmail.com>
2018-09-18 15:46:47 +0200

    boilerplate code

--- Go/CG/contests/cotc.go
+++ Go/CG/contests/cotc.go
@@ -2,32 +2,80 @@ package main
 
 import "fmt"
 
-//import "os"
+type actionType string
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
+const (
+	move   actionType = "MOVE"
+	wait   actionType = "WAIT"
+	slower actionType = "SLOWER"
+)
 
+type Point struct {
+	x, y int
+}
+
+type Player struct {
+	id    int
+	ships []Ship
+}
+type Ship struct {
+	pos         Point
+	orientation int
+	speed       int
+	rum         int
+	owner       int
+}
+
+type Barrel struct {
+	pos       Point
+	rumAmount int
+}
+
+type State struct {
+	entityCount int
+	players     [2]Player
+	ships       []Ship
+	barrels     []Barrel
+}
+
+//WIP
+type Turn struct{}
+
+func (s *State) readEntities() {
+	var entityCount int
+	fmt.Scan(&entityCount)
+
+	for i := 0; i < entityCount; i++ {
+		var entityId int
+		var entityType string
+		var x, y, arg1, arg2, arg3, arg4 int
+		fmt.Scan(&entityId, &entityType, &x, &y, &arg1, &arg2, &arg3, &arg4)
+		switch entityType {
+		case "SHIP":
+			if arg4 == 1 {
+				s.players[1].ships = append(s.players[1].ships, Ship{pos: Point{x, y}, orientation: arg1, speed: arg2, rum: arg3, owner: arg4})
+				s.ships = append(s.ships, Ship{pos: Point{x, y}, orientation: arg1, speed: arg2, rum: arg3, owner: arg4})
+			} else if arg4 == 0 {
+				s.players[0].ships = append(s.players[0].ships, Ship{pos: Point{x, y}, orientation: arg1, speed: arg2, rum: arg3, owner: arg4})
+				s.ships = append(s.ships, Ship{pos: Point{x, y}, orientation: arg1, speed: arg2, rum: arg3, owner: arg4})
+
+			}
+		case "BARREL":
+			s.barrels = append(s.barrels, Barrel{pos: Point{x, y}, rumAmount: arg1})
+		}
+
+	}
+
+}
 func main() {
+	agent := State{}
 	for {
 		// myShipCount: the number of remaining ships
 		var myShipCount int
 		fmt.Scan(&myShipCount)
 
-		// entityCount: the number of entities (e.g. ships, mines or cannonballs)
-		var entityCount int
-		fmt.Scan(&entityCount)
-
-		for i := 0; i < entityCount; i++ {
-			var entityId int
-			var entityType string
-			var x, y, arg1, arg2, arg3, arg4 int
-			fmt.Scan(&entityId, &entityType, &x, &y, &arg1, &arg2, &arg3, &arg4)
-		}
+		agent.readEntities()
 		for i := 0; i < myShipCount; i++ {
-
-			// fmt.Fprintln(os.Stderr, "Debug messages...")
 			fmt.Printf("MOVE 11 10\n") // Any valid action, such as "WAIT" or "MOVE x y"
 		}
 	}
