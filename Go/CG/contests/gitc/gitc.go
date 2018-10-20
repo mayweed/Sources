654e8dc8ed6d9e54722944c1a97c0aef2866ddd5
Guillaume Raimondeau <raimondeau@gmail.com>
2018-10-20 11:11:54 +0200

    suddenly pass wood1!! long needed overhaul to parse new input

--- Go/CG/contests/gitc/gitc.go
+++ Go/CG/contests/gitc/gitc.go
@@ -2,95 +2,103 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"sort"
 	"strings"
 )
 
-type Factory struct {
-	id         int
-	owner      int
-	cyborgs    int
-	production int
-}
-type Troop struct {
-	id             int
-	owner          int
-	from           int
-	to             int
-	cyborgs        int
-	remainingTurns int
-}
 type Link struct {
 	from     int
 	to       int
 	distance int
 }
 
-//Action+Turn should be mixed together??
-//Then a func that create a moves slice string
-//and send it. And GET RID of actionType
+type Factory struct {
+	id             int
+	owner          int
+	unitCount      int
+	productionRate int
+}
+type Troop struct {
+	id             int
+	owner          int
+	from           int
+	to             int
+	unitCount      int
+	remainingTurns int
+}
+type Bomb struct {
+	owner          int
+	from           int
+	to             int
+	remainingTurns int
+}
+
 type Action struct {
 	actionType  string
 	from        int
 	to          int
 	cyborgCount int
-}
-
-//should be append to moves in Turn should add ';'
-func (a Action) printAction() string {
-	var s string
-	if a.actionType == "move" {
-		s = fmt.Sprintf("MOVE %d %d %d", a.from, a.to, a.cyborgCount)
-	} else {
-		//if no move, just wait?
-		s = fmt.Sprintf("WAIT")
-	}
-	return s
-}
-
-type Turn struct {
-	possibleActions []Action
-	//encapsulate Action in string (sprintf)
-	moves []string
-}
-
-func (t Turn) sendCommands() {
-	cmd := "WAIT"
-	if len(t.moves) == 0 {
-		//log.Println("List of commands is empty, WAIT will be sent")
-	} else {
-		cmd = strings.Join(t.moves, ";")
-	}
-	fmt.Println(cmd)
+	msg         string
 }
 
 type Player struct {
 	factories []Factory
 	troops    []Troop
-	turn      Turn
+	bombs     []Bomb
+	action    Action
+	turn      []string
 	score     int
 }
 
 func (p Player) facWithMaxCyb() int {
 	var max, id int
 	for _, f := range p.factories {
-		if f.cyborgs > max {
-			max = f.cyborgs
+		if f.unitCount > max {
+			max = f.unitCount
 			id = f.id
 		}
 	}
 	return id
 }
-func (p *Player) calcScore() {
+func (p *Player) getScore() {
 	var total int
 	for _, f := range p.factories {
-		total += f.cyborgs
+		total += f.unitCount
 	}
 	for _, t := range p.troops {
-		total += t.cyborgs
+		total += t.unitCount
 	}
 	p.score = total
+}
+func (p *Player) move(from, to, cyborgCount int) {
+	p.action.actionType = "MOVE"
+	p.action.from = from
+	p.action.to = to
+	p.action.cyborgCount = cyborgCount
+	p.turn = append(p.turn, fmt.Sprintf("MOVE %d %d %d", p.action.from, p.action.to, p.action.cyborgCount))
+}
+func (p *Player) bomb(from, to int) {
+	p.action.actionType = "BOMB"
+	p.action.from = from
+	p.action.to = to
+	p.turn = append(p.turn, fmt.Sprintf("BOMB %d %d", p.action.from, p.action.to))
+}
+func (p *Player) wait() {
+	p.action.actionType = "WAIT"
+	p.turn = append(p.turn, fmt.Sprintf("WAIT"))
+}
+func (p *Player) msg(msg string) {
+	p.action.actionType = "MSG"
+	p.action.msg = msg
+	p.turn = append(p.turn, fmt.Sprintf("MSG %s", p.action.msg))
+}
+func (p *Player) sendCommands() {
+	if len(p.turn) == 0 {
+		p.wait()
+		p.msg("sending wait, turn empty")
+	}
+	cmd := strings.Join(p.turn, ";")
+	fmt.Println(cmd)
 }
 
 type State struct {
@@ -98,6 +106,8 @@ type State struct {
 	linkCount        int
 	links            []Link
 	neutralFactories []Factory
+	bombs            []Bomb
+	turn             int
 	me               Player
 	opp              Player
 }
@@ -107,9 +117,11 @@ func (s *State) sortLinksByDist() {
 	sort.Slice(s.links, func(i, j int) bool { return s.links[i].distance < s.links[j].distance })
 }
 func (s *State) sortFacByProd() {
-	sort.Slice(s.neutralFactories, func(i, j int) bool { return s.neutralFactories[i].production < s.neutralFactories[j].production })
-	sort.Slice(s.opp.factories, func(i, j int) bool { return s.opp.factories[i].production < s.opp.factories[j].production })
-	sort.Slice(s.me.factories, func(i, j int) bool { return s.me.factories[i].production < s.me.factories[j].production })
+	sort.Slice(s.neutralFactories, func(i, j int) bool {
+		return s.neutralFactories[i].productionRate < s.neutralFactories[j].productionRate
+	})
+	sort.Slice(s.opp.factories, func(i, j int) bool { return s.opp.factories[i].productionRate < s.opp.factories[j].productionRate })
+	sort.Slice(s.me.factories, func(i, j int) bool { return s.me.factories[i].productionRate < s.me.factories[j].productionRate })
 }
 
 func (s *State) readMap() {
@@ -141,32 +153,41 @@ func (s *State) readEntity() {
 		var entityType string
 		var arg1, arg2, arg3, arg4, arg5 int
 		fmt.Scan(&entityId, &entityType, &arg1, &arg2, &arg3, &arg4, &arg5)
-
-		if entityType == "FACTORY" && arg1 == 0 {
-			s.neutralFactories = append(s.neutralFactories, Factory{entityId, arg1, arg2, arg3})
-		} else if entityType == "FACTORY" && arg1 == 1 {
-			s.me.factories = append(s.me.factories, Factory{entityId, arg1, arg2, arg3})
-		} else if entityType == "FACTORY" && arg1 == -1 {
-			s.opp.factories = append(s.opp.factories, Factory{entityId, arg1, arg2, arg3})
-		}
-
-		if entityType == "TROOP" && arg1 == 1 {
-			s.me.troops = append(s.me.troops, Troop{entityId, arg1, arg2, arg3, arg4, arg5})
-		} else if entityType == "TROOP" && arg1 == -1 {
-			s.opp.troops = append(s.opp.troops, Troop{entityId, arg1, arg2, arg3, arg4, arg5})
+		switch entityType {
+		case "FACTORY":
+			switch arg1 {
+			case 0:
+				s.neutralFactories = append(s.neutralFactories, Factory{id: entityId, owner: arg1, unitCount: arg2, productionRate: arg3})
+			case 1:
+				s.me.factories = append(s.me.factories, Factory{id: entityId, owner: arg1, unitCount: arg2, productionRate: arg3})
+			case -1:
+				s.opp.factories = append(s.opp.factories, Factory{id: entityId, owner: arg1, unitCount: arg2, productionRate: arg3})
+			}
+		case "TROOP":
+			switch arg1 {
+			case 0:
+				s.me.troops = append(s.me.troops, Troop{entityId, arg1, arg2, arg3, arg4, arg5})
+			case -1:
+				s.opp.troops = append(s.opp.troops, Troop{entityId, arg1, arg2, arg3, arg4, arg5})
+			}
+		case "BOMB":
+			switch arg1 {
+			case 1:
+				s.me.bombs = append(s.me.bombs, Bomb{arg1, arg2, arg3, arg4})
+			case -1:
+				s.opp.bombs = append(s.opp.bombs, Bomb{arg1, arg2, arg3, arg4})
+			}
 		}
 	}
-	s.me.calcScore()
-	s.opp.calcScore()
+	s.me.getScore()
+	s.opp.getScore()
 	s.sortFacByProd()
-
-	log.Println(s.me.factories)
 }
 
 func (s *State) clearState() {
 	s.me.factories = []Factory{}
 	s.me.troops = []Troop{}
-	s.me.turn.moves = []string{}
+	s.me.turn = []string{}
 	s.neutralFactories = []Factory{}
 	s.opp.factories = []Factory{}
 }
@@ -190,30 +211,27 @@ func (s State) linkTo(f1, f2 Factory) bool {
 //factories...
 func (s *State) think() {
 	var needTroopsFactories []Factory
-	//var bestTurn Turn //will need that
 	for _, src := range s.me.factories {
 		//I should send troops to the factories!!
-		if src.cyborgs < 5 {
+		if src.unitCount < 5 {
 			needTroopsFactories = append(needTroopsFactories, src)
 			continue
 		}
 		if len(s.neutralFactories) != 0 {
 			for _, dest := range s.neutralFactories {
-				s.me.turn.moves = append(s.me.turn.moves, Action{"move", src.id, dest.id, 1}.printAction())
+				s.me.move(src.id, dest.id, 1)
 			}
 		} else {
 			for _, dest := range s.opp.factories {
-				s.me.turn.moves = append(s.me.turn.moves, Action{"move", src.id, dest.id, 1}.printAction())
+				s.me.move(src.id, dest.id, 1)
 			}
 		}
-		//log.Println(s.me.score)
 	}
-
 	//test to help does not work no? should think strat?
 	for _, dest := range needTroopsFactories {
 		for _, src := range s.me.factories {
-			if src.cyborgs > 5 {
-				s.me.turn.moves = append(s.me.turn.moves, Action{"move", src.id, dest.id, 1}.printAction())
+			if src.unitCount > 5 {
+				s.me.move(src.id, dest.id, 1)
 			}
 		}
 	}
@@ -225,7 +243,7 @@ func main() {
 	for {
 		board.readEntity()
 		board.think()
-		board.me.turn.sendCommands()
+		board.me.sendCommands()
 		board.clearState()
 	}
 }
