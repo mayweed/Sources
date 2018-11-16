b1294f7e24711ef63c231a5e4a6724a4edbd94d5
Guillaume Raimondeau <raimondeau@gmail.com>
2018-11-16 15:32:46 +0100

    un fichier à compiler

---
+++ C/CG/trietel.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 21
+
+typedef struct node{
+    int value;
+    struct node *next;
+}node;
+
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    
+    char telephone[MAX_LENGTH];
+    char *nums[N];
+    for (int i = 0; i < N; i++) 
+    {
+        scanf("%s", telephone);
+		nums[N]=&telephone;
+    }
+
+    for (int i=0;i<MAX_LENGTH-1;i++)
+    {
+        node *num = malloc(sizeof(int));   
+        num->value=atoi(telephone[i]);
+    }
+
+    //fprintf(stderr, "Debug messages...\n");
+
+    // The number of elements (referencing a number) stored in the structure.
+    printf("number\n");
+
+    return 0;
+}
