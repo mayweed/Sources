7326305a6f6c0dc867da1243fa3b91e9bb9204ba
Guillaume Raimondeau <raimondeau@gmail.com>
2018-12-11 22:10:55 +0100

    it mainly works, stays very weak, could be easily cornered if there is no path to quest. I have to write a decent printTurn() now. And if time a little sim to push correctly and help myself...

--- Go/CG/contests/xmasrush/xmasrush.go
+++ Go/CG/contests/xmasrush/xmasrush.go
@@ -5,16 +5,31 @@ import (
 	"log"
 )
 
-/* not good
 const (
-	UP    = Point{0, -1}
-	RIGHT = Point{+1, 0}
-	DOWN  = Point{0, +1}
-	LEFT  = Point{-1, 0}
+	MAP_WIDTH = 7
 )
-*/
+
 type Point struct {
 	x, y int
+}
+
+//P start Point, p2 where he wants to go
+func (p Point) printDirection(p2 Point) string {
+	var dir string
+	//return directly output "missing return at the end of func"...
+	if p2.x > p.x {
+		dir = "RIGHT"
+	}
+	if p2.x < p.x {
+		dir = "LEFT"
+	}
+	if p2.y < p.y {
+		dir = "UP"
+	}
+	if p2.y > p.y {
+		dir = "DOWN"
+	}
+	return dir
 }
 
 type Grid [7][7]Tile
@@ -35,33 +50,65 @@ type Player struct {
 }
 
 type Turn struct {
-	turnType int
+	turnType   int
+	directions []string
 	//action Action
 }
 
 type State struct {
-	players  [2]Player
-	grid     Grid
-	numItems int
-	turn     Turn
+	players   [2]Player
+	grid      Grid
+	numItems  int
+	itemTiles []Tile
+	turn      Turn
+}
+
+//this works in wood 2 I suppose, should refine by calculating which one is the
+//nearest from current pos...in this case the quest is passed in argument!!
+//should definitely REWRITE it!!
+func (s *State) getItemTilesPos(p Player) Tile {
+	var quest = s.players[0].quests[0]
+	var tile Tile
+	for _, tile = range s.itemTiles {
+		//dont forget the player id
+		if tile.itemName == quest && tile.itemPlayerId == 0 {
+			break
+		}
+	}
+	return tile
 }
 
 //golang yields the ascii code not the num...it's a quickfix...
 //see first answer here: https://stackoverflow.com/questions/15018545/how-to-index-characters-in-a-golang-string
+//UGLY CODE, will be non understandable in 6 weeks from now
 func (s State) getNeighbours(t Tile) []Tile {
 	var neighbours []Tile
 	//check borders!!
-	if t.direction[0] == 49 {
-		neighbours = append(neighbours, s.grid[t.position.y-1][t.position.x])
+	//to be qualified as neighbour one must be able to communicate
+	//if UP is 1 on my tile, the upper tile is a neighbour if and only if down is
+	//open!!
+	if t.position.y-1 > 0 && t.position.y-1 < 7 {
+		if t.direction[0] == 49 && s.grid[t.position.y-1][t.position.x].direction[2] == 49 {
+			neighbours = append(neighbours, s.grid[t.position.y-1][t.position.x])
+		}
 	}
-	if t.direction[1] == 49 {
-		neighbours = append(neighbours, s.grid[t.position.y][t.position.x+1])
+	//if RIGHT (direction 1) is ok, check left (dir 3) on the neighbouring cell
+	if t.position.x+1 > 0 && t.position.x+1 < 7 {
+		if t.direction[1] == 49 && s.grid[t.position.y][t.position.x+1].direction[3] == 49 {
+			neighbours = append(neighbours, s.grid[t.position.y][t.position.x+1])
+		}
 	}
-	if t.direction[2] == 49 {
-		neighbours = append(neighbours, s.grid[t.position.y+1][t.position.x])
+	//if DOWN is oki, the neighbouring must have up oki
+	if t.position.y+1 > 0 && t.position.y+1 < 7 {
+		if t.direction[2] == 49 && s.grid[t.position.y+1][t.position.x].direction[0] == 49 {
+			neighbours = append(neighbours, s.grid[t.position.y+1][t.position.x])
+		}
 	}
-	if t.direction[3] == 49 {
-		neighbours = append(neighbours, s.grid[t.position.y][t.position.x-1])
+	//if LEFT is oki neighbour must have right!!
+	if t.position.x-1 > 0 && t.position.x-1 < 7 {
+		if t.direction[3] == 49 && s.grid[t.position.y][t.position.x-1].direction[1] == 49 {
+			neighbours = append(neighbours, s.grid[t.position.y][t.position.x-1])
+		}
 	}
 	return neighbours
 }
@@ -111,6 +158,7 @@ func (s *State) read() {
 			s.grid[itemY][itemX].hasItem = true
 			s.grid[itemY][itemX].itemName = itemName
 			s.grid[itemY][itemX].itemPlayerId = itemPlayerId
+			s.itemTiles = append(s.itemTiles, s.grid[itemY][itemX])
 		}
 	}
 
@@ -133,12 +181,12 @@ func (s *State) read() {
 
 //a bfs?
 //Should yse a Tile!!or write Point -> tile
-func (s *State) bfsPath(playerPos, questPos Point) []Point {
-	var visited = make(map[Point]bool)
-	visited[playerPos] = true
+func (s *State) bfsPath(playerTile, questTile Tile) []Tile {
+	var visited = make(map[Tile]bool)
+	visited[playerTile] = true
 
-	var startTile = []Point{playerPos}
-	var queue = [][]Point{startTile}
+	var startTile = []Tile{playerTile}
+	var queue = [][]Tile{startTile}
 
 	for 0 < len(queue) {
 		//pop the first element
@@ -146,7 +194,7 @@ func (s *State) bfsPath(playerPos, questPos Point) []Point {
 		queue = queue[1:]
 
 		lastTile := path[len(path)-1]
-		if lastTile == questPos {
+		if lastTile == questTile {
 			return path
 		}
 
@@ -167,21 +215,38 @@ func (s *State) think() {
 		fmt.Println("PUSH 3 RIGHT") // PUSH <id> <direction> | MOVE <direction> | PASS
 	} else {
 		//s.bfsPath()
-		fmt.Println("MOVE RIGHT")
+		var path = s.bfsPath(s.grid[s.players[0].position.y][s.players[0].position.x], s.getItemTilesPos(s.players[0]))
+		if len(path) == 0 {
+			//=> move in the direction if possible or pass
+			fmt.Println("MOVE RIGHT")
+		} else { //test purpose if len(path) < 20 {
+			//go for it directly!! Indeed, try to...
+			for x, p := range path {
+				//that or a queue...or an index??
+				if x+1 < len(path) {
+					dir := p.position.printDirection(path[x+1].position)
+					s.turn.directions = append(s.turn.directions, dir)
+				}
+			}
+		}
 	}
-
 }
+
 func main() {
 
 	for {
 		//clean state to begin with
 		s := State{}
 		s.read()
-		//log.Println(s.players[0].quests)
-		t := s.getNeighbours(s.grid[3][3])
-		log.Println(s.grid[3][3].direction, t)
-
 		s.think()
 		//s.printTurn()
+
+		//TEST LOGS
+		//log.Println(s.players[0].quests)
+		log.Println(s.getItemTilesPos(s.players[0]))
+		log.Println(s.getNeighbours(s.grid[s.players[0].position.y][s.players[0].position.x]))
+		log.Println(s.bfsPath(s.grid[s.players[0].position.y][s.players[0].position.x], s.getItemTilesPos(s.players[0])))
+		log.Println(s.turn.directions)
+
 	}
 }
