b254bbe89a0c3137ac419c881c30dfe9c9ec94cc
Guillaume Raimondeau <raimondeau@gmail.com>
2018-12-20 09:07:15 +0100

    get it right this time!! RUST not GO!!

--- Rust/CG/practice/hard/roller_coaster.rs
+++ Rust/CG/practice/hard/roller_coaster.rs
@@ -1,15 +1,5 @@
 use std::io;
-use std::time::Instant;
 use std::collections::HashMap;
-
-macro_rules! print_err {
-    ($($arg:tt)*) => (
-        {
-            use std::io::Write;
-            writeln!(&mut ::std::io::stderr(), $($arg)*).ok();
-        }
-    )
-}
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
@@ -22,55 +12,58 @@ fn main() {
     let l = parse_input!(inputs[0], i32);
     let c = parse_input!(inputs[1], i32);
     let n = parse_input!(inputs[2], i32);
+    eprintln!("l {}, c {}, n {}",l,c,n);
     
     let mut queue:Vec<i64>=Vec::new();
     let mut remaining_places=&l;
-    let mut cache= HashMap::new();
     
-    for i in 0..n as usize{
+    //the first i64 == num of ppl in group, the second == dirhams per ride
+    let mut cache:HashMap<i64, i64> = HashMap::new();
+    
+    for i in 0..n as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let pi = parse_input!(input_line, i64);
         queue.push(pi);
     }
-    
     //vec types are indexed on usize
     let mut index:usize=0;
     //can't be i32, does not work (5th test overflow i32!)
     let mut cash_earned:i64=0;
     
-    print_err!("Num of places {}, num of times per day {}, num of groups {}",l,c,n);    
+    eprintln!("Num of places {}, num of times per day {}, num of groups {}",l,c,n);    
     
-    let now=Instant::now();
     for run in 0..c{
         let mut dirhams_ride:i64=0;
         let mut remaining_places:i64=l as i64;
-        //what the hell to cache?
         let mut num_groups=0;
+        let mut num_ppl_groups=0;
         
         loop{
             if index as i32 >= n{index=0};
             //test 4 come on!!
             if l > n && num_groups==n{break};
-            
             if remaining_places-queue[index] < 0{
                 break
             }else{
                 remaining_places -= queue[index];
+                //tu regardes si on a déjà cette valeur et tu l'ajoutes direct
+                // match cache.get(remaining_places){
+                //Some(remaining_places) => cash_earned + dirhams ride
+                //else valeur classique
                 dirhams_ride+=queue[index];
-                num_groups+=1;
                 index+=1;
+                num_groups+=1;
+                num_ppl_groups=remaining_places;
+                eprintln!("numGroups {}, pplGroups {}, cash per ride {}, total {}",num_groups,num_ppl_groups,dirhams_ride,cash_earned);
+                cache.insert(remaining_places,dirhams_ride);
+               
             }
             
         }
         cash_earned+=dirhams_ride;
-        cache.insert (index,dirhams_ride);
-        //TEST on case 6...
-        if num_groups==190{
-        print_err!("index {},cash per ride {}, total {} cache: {:?}",index,dirhams_ride,cash_earned,cache);
-        }
     }
-    let end=now.elapsed();
-    print_err!("Elapsed: {} ms",(end.as_secs() * 1_000) + (end.subsec_nanos() / 1_000_000) as u64);
+    eprintln!("{:?}",cache);
+
     println!("{}",cash_earned);
 }
