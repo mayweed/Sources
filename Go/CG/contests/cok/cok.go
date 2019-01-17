c56bfda4e5847ea5eb4d461551b280f65d39d462
Guillaume Raimondeau <raimondeau@gmail.com>
2019-01-17 15:30:02 +0100

    lots of thing to do...just played with the map etc...

--- Go/CG/contests/cok/cok.go
+++ Go/CG/contests/cok/cok.go
@@ -23,20 +23,36 @@ type Entity struct {
 	param2 int
 }
 
+//quick
+func turnWait() string {
+	return "WAIT"
+}
+func turnMove(c Cell) string {
+	s := fmt.Sprintf("MOVE %d %d", c.x, c.y)
+	return s
+}
+
 type State struct {
-	board      [][]Cell
-	wanderers  []Entity
-	explorers  []Entity
-	emptyCells []Cell
+	mapHeight      int
+	mapWidth       int
+	board          [][]Cell
+	visited        map[Cell]bool
+	me             Entity
+	wanderers      []Entity
+	explorers      []Entity
+	wandererSpawns []Cell
+	turn           int
 }
 
 //should test linear+create a file map reusable!!
 func (s *State) initBoard() {
 	var width int
 	fmt.Scan(&width)
+	s.mapWidth = width
 
 	var height int
 	fmt.Scan(&height)
+	s.mapHeight = height
 
 	s.board = make([][]Cell, height)
 
@@ -47,8 +63,8 @@ func (s *State) initBoard() {
 		for x := range s.board[y] {
 			item := strings.Split(row, "")
 			s.board[y][x] = Cell{x: x, y: y, what: item[x]}
-			if item[x] == "." {
-				s.emptyCells = append(s.emptyCells, Cell{x: x, y: y, what: item[x]})
+			if item[x] == "w" {
+				s.wandererSpawns = append(s.wandererSpawns, Cell{x: x, y: y, what: item[x]})
 			}
 		}
 	}
@@ -68,10 +84,63 @@ func (s State) printBoard() {
 	log.Println(row)
 }
 
+//for a given cell got the neighbours
+func (s State) getNeighbours(c Cell) []Cell {
+	var neighbours []Cell
+	//for _, cell := range s.board {
+	if s.board[c.y+1][c.x].what == "." && c.y+1 < s.mapHeight {
+		neighbours = append(neighbours, s.board[c.y+1][c.x])
+	}
+	if s.board[c.y-1][c.x].what == "." && c.y-1 > 0 {
+		neighbours = append(neighbours, s.board[c.y-1][c.x])
+	}
+	if s.board[c.y][c.x+1].what == "." && c.x+1 < s.mapWidth {
+		neighbours = append(neighbours, s.board[c.y][c.x+1])
+	}
+	if s.board[c.y][c.x-1].what == "." && c.x-1 > 0 {
+		neighbours = append(neighbours, s.board[c.y][c.x-1])
+	}
+	//}
+	return neighbours
+}
+
+func (s *State) think() string {
+	//simply go to next free?
+	var freePos = s.getNeighbours(s.me.pos)
+	var action string
+	//should score which one is best??? And REALLY, which one is best?? :)))
+	if len(freePos) == 0 {
+		action = turnWait()
+		s.visited[s.me.pos] = true
+	} else {
+		if s.turn == 0 {
+			action = turnMove(freePos[1])
+			s.visited[freePos[1]] = true
+		} else {
+			//should factorize that
+			//and examine all cells!!
+			for _, cell := range freePos {
+				if !s.visited[cell] {
+					action = turnMove(cell)
+					s.visited[cell] = true
+					break
+				} else {
+					//am cornered!!
+					action = turnWait()
+				}
+			}
+		}
+	}
+	return action
+}
+
 func main() {
-	gState := State{}
-	gState.initBoard()
-	gState.printBoard()
+	s := State{}
+	//is this really a good idea in the end??
+	s.visited = make(map[Cell]bool)
+
+	s.initBoard()
+	s.printBoard()
 
 	// sanityLossLonely: how much sanity you lose every turn when alone, always 3 until wood 1
 	// sanityLossGroup: how much sanity you lose every turn when near another player, always 1 until wood 1
@@ -91,15 +160,21 @@ func main() {
 			fmt.Scan(&entityType, &id, &x, &y, &param0, &param1, &param2)
 			switch entityType {
 			case "EXPLORER":
-				gState.explorers = append(gState.explorers, Entity{id: id, pos: Cell{x: x, y: y, what: entityType}, param1: param1, param2: param2})
+				if i == 0 {
+					s.me = Entity{id: id, pos: Cell{x: x, y: y, what: entityType}, param1: param1, param2: param2}
+				}
+				s.explorers = append(s.explorers, Entity{id: id, pos: Cell{x: x, y: y, what: entityType}, param1: param1, param2: param2})
 			case "WANDERER":
-				gState.wanderers = append(gState.wanderers, Entity{id: id, pos: Cell{x: x, y: y, what: entityType}, param1: param1, param2: param2})
+				s.wanderers = append(s.wanderers, Entity{id: id, pos: Cell{x: x, y: y, what: entityType}, param1: param1, param2: param2})
 			}
 		}
-		log.Println(gState.explorers)
-		fmt.Println("WAIT") // MOVE <x> <y> | WAIT
+		log.Println(s.visited)
+		log.Println(s.getNeighbours(s.me.pos))
+		output := s.think()
+		fmt.Println(output)
 
-		gState.explorers = []Entity{}
-		gState.wanderers = []Entity{}
+		s.explorers = []Entity{}
+		s.wanderers = []Entity{}
+		s.turn += 1
 	}
 }
