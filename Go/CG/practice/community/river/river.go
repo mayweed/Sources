e3b8a4101422f7aa758f061c97fbe1b046e3999c
Guillaume Raimondeau <raimondeau@gmail.com>
2019-01-26 09:26:36 +0100

    I began this one months ago, might finish it once...

---
+++ Go/CG/practice/community/river/river.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	//"log"
+)
+
+func countNum(num int) int {
+	//base case
+	if num < 10 {
+		return num
+	} else {
+		return num%10 + countNum(num/10)
+	}
+}
+
+//for a num give the next one in line
+//create an anonymous func?? to got river?
+func river(num int) int {
+	return num + countNum(num)
+}
+
+func main() {
+	var r1 int
+	fmt.Scan(&r1)
+
+	var r2 int
+	fmt.Scan(&r2)
+
+	//log.Println(r1,r2)
+	fmt.Println(river(32))
+	fmt.Println(42)
+}
