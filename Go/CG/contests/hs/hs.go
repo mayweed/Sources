7a88977f241173ac1626a86e9a3c2162a31af356
Guillaume Raimondeau <raimondeau@gmail.com>
2019-06-20 18:08:40 +0200

    Began to think about select moves amd place to bomb

--- Go/CG/contests/hs/hs.go
+++ Go/CG/contests/hs/hs.go
@@ -3,7 +3,11 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"log"
+	"math"
+	"math/rand"
 	"strings"
+	"time"
 )
 
 const (
@@ -14,6 +18,8 @@ const (
 	PLAYER     = 0
 	BOMB       = 1
 	CRATE      = 2
+	//wood league
+	BOMB_RANGE = 3
 	//actionType
 	m actionType = 3
 	b actionType = 4
@@ -51,11 +57,24 @@ type Turn struct {
 	//evalScore float64
 }
 type State struct {
-	myId    int //who i am?
+	myId    int  //who i am?
+	me      Cell //where i am
 	board   Grid
 	crates  []Cell
-	bombs   []Entity
-	players []Entity
+	bombs   []Cell
+	players []Cell
+}
+
+func (s *State) cratesAround(c Cell) int {
+	var numCrates int
+	//for any given free cell let's see how many crates are in range
+	for _, crate := range s.crates {
+		if c.x == crate.x && math.Round(math.Abs(float64(c.y-crate.y))) <= BOMB_RANGE ||
+			c.y == crate.y && math.Round(math.Abs(float64(c.x-crate.x))) <= BOMB_RANGE {
+			numCrates += 1
+		}
+	}
+	return numCrates
 }
 
 //should make it generic, so that it works for every player
@@ -96,25 +115,29 @@ func (s *State) readEntities() {
 	var entities int
 	fmt.Scan(&entities)
 
-	s.bombs = []Entity{}
-	s.players = []Entity{}
+	s.bombs = []Cell{}
+	s.players = []Cell{}
 
 	for i := 0; i < entities; i++ {
 		var entityType, owner, x, y, param1, param2 int
 		fmt.Scan(&entityType, &owner, &x, &y, &param1, &param2)
-		switch entityType {
-		case 0:
-			s.board[x][y].what.entityType = entityType
-			s.board[x][y].what.owner = owner
-			s.board[x][y].what.param1 = param1
-			s.board[x][y].what.param2 = param2
-			s.players = append(s.players, Entity{entityType, owner, param1, param2})
-		case 1:
-			s.board[x][y].what.entityType = entityType
-			s.board[x][y].what.owner = owner
-			s.board[x][y].what.param1 = param1
-			s.board[x][y].what.param2 = param2
-			s.bombs = append(s.bombs, Entity{entityType, owner, param1, param2})
+		if owner == s.myId {
+			s.me = Cell{x: x, y: y, what: Entity{entityType, owner, param1, param2}}
+		} else {
+			switch entityType {
+			case 0:
+				s.board[x][y].what.entityType = entityType
+				s.board[x][y].what.owner = owner
+				s.board[x][y].what.param1 = param1
+				s.board[x][y].what.param2 = param2
+				s.players = append(s.players, Cell{x: x, y: y, what: Entity{entityType, owner, param1, param2}})
+			case 1:
+				s.board[x][y].what.entityType = entityType
+				s.board[x][y].what.owner = owner
+				s.board[x][y].what.param1 = param1
+				s.board[x][y].what.param2 = param2
+				s.bombs = append(s.bombs, Cell{x: x, y: y, what: Entity{entityType, owner, param1, param2}})
+			}
 		}
 
 	}
@@ -144,9 +167,22 @@ func (s State) printBoard() string {
 	return result.String()
 }
 
-func think() {
-	//so first move to a cell
+func (s *State) think() {
+	//first select a batch of random possible move
+	//evaluate them: is this enough far from any given bomb? is this close to
+	// a foe (bombs will become lethal)? AND is there any crates around?
 
+	var cells []Cell
+
+	//so first move to a random cell
+	//select 10 cells check cratesAround
+	for i := 0; i < 10; i++ {
+		x := rand.Intn(12)
+		y := rand.Intn(10)
+		cells = append(cells, s.board[x][y])
+	}
+
+	log.Println(cells)
 	//then pose a bomb
 
 	//then get out of bomb range
@@ -156,16 +192,21 @@ func think() {
 	//and loop til no bombs left on map?
 }
 func main() {
+	rand.Seed(time.Now().Unix())
 	s := State{}
-
 	for {
 		s.readGrid()
 		s.readEntities()
 
+		s.think()
 		//MOVE test
 		//should list possible moves+simulate where to leave bombs to get
 		//more boxes destroy
-		fmt.Println("MOVE 11 13") // Write action to stdout
+		fmt.Println("MOVE 10 10") // Write action to stdout
+
+		//LOGS
+		//num := s.cratesAround(s.board[x][y])
+		//log.Println("param1:", s.me.what.param1) //, "x:", x, "y:", y, "num", num)
 
 	}
 }
