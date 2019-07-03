f2c6317a58cec1004facb339423c633bcf8b0845
Guillaume Raimondeau <raimondeau@gmail.com>
2019-07-03 18:05:36 +0200

    playing again...

--- php/horseRaceHyperD.php
+++ php/horseRaceHyperD.php
@@ -8,6 +8,9 @@ function distance ($v1, $e1,$v2,$e2){
 fscanf(STDIN, "%d",
     $N
 );
+
+$min= 10000000;
+
 for ($i = 0; $i < $N; $i++)
 {
     fscanf(STDIN, "%d %d",
@@ -20,9 +23,24 @@ for ($i = 0; $i < $N; $i++)
             "v" =>$V,
             "e" => $E
             );
-    //if ($i >= 1)
-    //calculate dist between $t[$i] and $t[$i-1]
-    //$dist = distance($t[$i]["v"],$t[$i]["e"],$t[$i-1]["v"],$t[$i-1]["e"])
+            
+    
+    
+    //algo wrong: you must compare each element: $t[0] to $t[1] -> $t[9]
+    //$t[1] to all elements above and the one below etc...
+    //this is not so simple...
+    if ($i >= 1){
+        //calculate dist between $t[$i] and $t[$i-1]   
+        $dist =  distance($t[$i]["v"],$t[$i]["e"],$t[$i-1]["v"],$t[$i-1]["e"]);
+        if ($dist < $min){
+            $min=$dist;
+        }
+        echo("$dist\n");
+        echo("$min\n");
+    }
+        
+    //put dist in an array and sort the array in ascending order (arr[0]= answer)
+    
 }
 
 // Write an action using echo(). DON'T FORGET THE TRAILING \n
@@ -30,5 +48,7 @@ for ($i = 0; $i < $N; $i++)
 
 //var_dump($t);
 //echo("42\n");
+
+    echo("$min\n");
 echo ($t[2]["v"]);
 ?>
