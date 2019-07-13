97310850522ed7962ad1f45b5e5753a7f439f08e
Guillaume Raimondeau <raimondeau@gmail.com>
2019-07-13 13:21:59 +0200

    tinkering

--- Rust/CG/practice/hard/roller_coaster.rs
+++ Rust/CG/practice/hard/roller_coaster.rs
@@ -1,11 +1,10 @@
 use std::io;
-use std::collections::HashMap;
-use std::time::{Duration,Instant};
 
 //i think i got the algo: first you take the groups and establish a gain maps of group index ->
 //gain
 //THEN you apply it numOfTime times
 //this way gains are pre-calculated...
+//Key is to store two maps / arrays (one of earns per group and one for index of group which goes next) and then just jump here and there and compute total.
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
@@ -21,9 +20,6 @@ fn main() {
     let mut groups:Vec<i64>=Vec::new();
     let mut remaining_places=&places;
     
-    //the first i64 == num of ppl in group, the second == dirhams per ride
-    let mut cache:HashMap<i64, i64> = HashMap::new();
-    
     //fill the seats on demand here
     let mut dirhams_ride:i64=0;
     let mut remaining_places:i64=places as i64;
@@ -33,28 +29,17 @@ fn main() {
         io::stdin().read_line(&mut input_line).unwrap();
         let pi = parse_input!(input_line, i64);
         groups.push(pi);
-        /*
-        if remaining_places - pi < 0{
-            break;
-        }else{
-            remaining_places -= pi;
-            dirhams_ride+=pi;
-            //cache[&(i as i64]=dirhams_ride;
-            cache.insert(i as i64,dirhams_ride);
-        }
-        */
     }
-    eprintln!("{:?}",cache);
+    
     //vec types are indexed on usize
     let mut index:usize=0;
     //can't be i32, does not work (5th test overflow i32!)
-    let mut cash_earned:i64=0;
+    let mut cash_earned:Vec<i64>;
     
     eprintln!("Num of places {}, num of times per day {}, groups {:?}",places,numOfTime,groups);    
     
-    let now= Instant::now();
     for run in 0..numOfTime{
-                let mut num_groups=0;
+        let mut num_groups=0;
         let mut num_ppl_groups=0;
         
         loop{
@@ -69,8 +54,8 @@ fn main() {
                 index+=1;
                 num_groups+=1;
                 num_ppl_groups=remaining_places;
-                eprintln!("numGroups {}, pplGroups {}, cash per ride {}, total {}",num_groups,num_ppl_groups,dirhams_ride,cash_earned);
-                cache.insert(remaining_places,dirhams_ride);
+                //eprintln!("numGroups {}, pplGroups {}, cash per ride {}, total {}",num_groups,num_ppl_groups,dirhams_ride,cash_earned);
+                cash_earned[index]=dirhams_ride;
                
             }
             
@@ -78,9 +63,5 @@ fn main() {
         cash_earned+=dirhams_ride;
     }
 
-    let new_now= Instant::now();
-    eprintln!("{:?}",new_now.duration_since(now));
-    //eprintln!("{:?}",cache);
-    
     println!("{}",cash_earned);
 }
