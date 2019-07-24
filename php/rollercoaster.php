7a360fa7ea3d2f289f9251d431bc27fe8a1844ae
Guillaume Raimondeau <raimondeau@gmail.com>
2019-07-24 18:13:07 +0200

    so i have an array of gain / index of next group, now need to find a way to "jump here and there" in php

--- php/rollercoaster.php
+++ php/rollercoaster.php
@@ -2,6 +2,10 @@
 //Key is to store two maps / arrays (one of earns per group and one for index of
 //group which goes next) and then just jump here and there and compute total.
 //
+class Gain {
+    public static $index; //index of the next group
+    public static $cash; //cash til here
+}
 fscanf(STDIN, "%d %d %d", $L, $C, $N);
 
 $places = $L;
@@ -13,17 +17,34 @@ for ($i = 0; $i < $N; $i++) {
     $groups[$i] = $pi;
 }
 
-$cash = 0;
 
-foreach ($groups as $index => $group) {
-    if ($group - $places < 0) {
-        $gain[$index]=$cash;
-        //next group = index+1;?
-        $places=$L;
-    } else {
-        $cash+=$group;
-        $places -= $group;
+//first the gain per group and the index of the next group
+for ($i = 0; $i <= $numGroups-1; $i++){
+    $g = new Gain();
+    $numPplPerRide= 0;
+    $index =$i; //of the NEXT group to go there directly
+    while ($numPplPerRide+$groups[$index] - $places <= 0) {
+        $numPplPerRide+=$groups[$index]; //$i ou $index?
+
+        //retour de l'index à zéro si index dépasse i (on est arrivé au bout de la liste)
+        if ($index < $numGroups-1){
+            $index++;
+        }else{
+            $index =0;
+        }
+
+        //gestion des doublons??
+        if ($index == $i){
+            break;
+        }
     }
+    $gain[$i]= array(
+        $index => $numPplPerRide
+    );
+    //$g -> $index = $index;
+    //$g -> $cash = $numPplPerRide;
+    //var_dump("$numGroups,$i,$index,$numPplPerRide\n");
+    var_dump($gain);
 }
 
-echo("$cash\n");
+echo("7\n");
