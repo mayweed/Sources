3339a0404a62d3a585719e6884593f2c0b688770
Guillaume Raimondeau <raimondeau@gmail.com>
2019-08-10 15:06:34 +0200

    Setting the stage for the new problem of the week ;)

---
+++ php/CG/medium/community/battleship.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+fscanf(STDIN, "%s",
+    $SHOT
+);
+for ($i = 0; $i < 10; $i++)
+{
+    fscanf(STDIN, "%s",
+        $LINE
+    );
+}
+
+// Write an action using echo(). DON'T FORGET THE TRAILING \n
+// To debug (equivalent to var_dump): error_log(var_export($var, true));
+
+echo("answer\n");
+?>
