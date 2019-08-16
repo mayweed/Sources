461b580e67c301786fd45b68fa5994eea3d8a4ff
Guillaume Raimondeau <raimondeau@gmail.com>
2019-08-16 14:36:09 +0200

    4 and 6 keep failing

--- Go/CG/practice/community/river/river.go
+++ Go/CG/practice/community/river/river.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"log"
+	//"log"
 )
 
 func countNum(num int) int {
@@ -20,6 +20,18 @@ func river(num int) int {
 	return num + countNum(num)
 }
 
+//is the last elt of list 2 in list 1?
+func In(n int, l []int) int {
+	var m int
+	for _, e := range l {
+		if e == n {
+			m = e
+			break
+		}
+	}
+	return m
+}
+
 func main() {
 	var r1 int
 	fmt.Scan(&r1)
@@ -32,15 +44,20 @@ func main() {
 	var y = r2
 	var riverX []int
 	var riverY []int
+	riverX = append(riverX, r1)
+	riverY = append(riverY, r2)
+	var res int
 	for {
 		riverX = append(riverX, river(x))
 		riverY = append(riverY, river(y))
-		//if x == y {
-		//break
-		//}
+
+		if ok := In(riverX[len(riverX)-1], riverY); ok != 0 {
+			res = ok
+			break
+		}
 		x = riverX[len(riverX)-1]
 		y = riverY[len(riverY)-1]
-		log.Println(riverX, y)
+		//log.Println(riverX,riverY)
 	}
-	fmt.Println(x)
+	fmt.Println(res)
 }
