94e7f38dc8025184cea4cc7cc175472e1aa33c98
Guillaume Raimondeau <raimondeau@gmail.com>
2020-03-28 18:57:57 +0100

    adapt to silence/sonar + targets which dont work

--- Go/CG/contests/ooc/ooc.go
+++ Go/CG/contests/ooc/ooc.go
@@ -16,48 +16,50 @@ const (
 	WIDTH  = 15
 )
 
+//POINT
 //think about type Point in import "image"
 type Point struct {
 	x, y int
 }
 
-func (s *State) getTileFromPoint(p Point) Tile {
-	return s.carte[p.x][p.y]
-}
-
 //idea where is the last torpedo pos located??
-func (s *State) getPosBySector(p Point) {
+func getPosBySector(p Point) int {
+	var zone int
 	if p.x >= 0 && p.x <= 4 && p.y > 0 && p.y <= 4 {
-		s.opp.enemyZone = 1
+		zone = 1
 	}
 	if p.x >= 0 && p.x <= 4 && p.y > 4 && p.y <= 9 {
-		s.opp.enemyZone = 4
+		zone = 4
 	}
 	if p.x >= 0 && p.x <= 4 && p.y > 9 && p.y <= 14 {
-		s.opp.enemyZone = 7
+		zone = 7
 	}
 	if p.x > 4 && p.x <= 9 && p.y > 0 && p.y <= 4 {
-		s.opp.enemyZone = 2
+		zone = 2
 	}
 	if p.x > 4 && p.x <= 9 && p.y > 4 && p.y <= 9 {
-		s.opp.enemyZone = 5
+		zone = 5
 	}
 	if p.x > 4 && p.x <= 9 && p.y > 9 && p.y <= 14 {
-		s.opp.enemyZone = 8
+		zone = 8
 	}
 	if p.x > 9 && p.x <= 14 && p.y > 0 && p.y <= 4 {
-		s.opp.enemyZone = 3
+		zone = 3
 	}
 	if p.x > 9 && p.x <= 14 && p.y > 4 && p.y <= 9 {
-		s.opp.enemyZone = 6
+		zone = 6
 	}
 	if p.x > 9 && p.x <= 14 && p.y > 9 && p.y <= 14 {
-		s.opp.enemyZone = 9
+		zone = 9
 	}
-
+	return zone
 }
 
-//a graph might help?
+//TILE
+//getTile() with limit checking
+//to see what happen if border crossed?
+//https://rosettacode.org/wiki/Bitmap/Flood_fill#Go ==> would like to see the getpx func i imagine a getTile()
+// with limit checkers in a Grid Type...
 type Tile struct {
 	pos   Point
 	what  string
@@ -65,91 +67,124 @@ type Tile struct {
 }
 
 func isWalkable(t Tile) bool {
-	/* does not work with array...
-	if t.pos.x >= 0 && t.pos.x < WIDTH && t.pos.y >= 0 && t.pos.y < HEIGHT && t.what == "." {
-		return true
-	}
-	return false
-	*/
 	return t.what == "."
 }
 
-type Me struct {
-	id              int
-	currentPos      Tile
-	hitPoints       int
-	canGoWest       bool
-	canGoEast       bool
-	canGoNorth      bool
-	canGoSouth      bool
-	torpedoCooldown int
-	oppSurfaceHint  string
-}
-
+//OPP
 type Opp struct {
-	hitPoints    int
-	oppDirection string
-	torpedoPos   []Point
-	enemyZone    int
+	hitPoints       int
+	oppDirection    string
+	torpedoPos      []Point
+	lastTorpedoZone int
+	surfaceZone     int
+	sonarZone       int
+	dirs            string
 }
 
-func (s *State) parseOppOrders(orders string) {
+func (o *Opp) parseOppOrders(orders string) {
 	//sanitize orders
 	st := strings.ReplaceAll(orders, "|", " ")
 	//split it
 	ord := strings.Split(st, " ")
 	for idx, w := range ord {
 		if w == "N" || w == "E" || w == "W" || w == "S" {
-			s.opp.oppDirection = w
+			o.oppDirection = w
+			//a string of ALL dir followed by my opp
+			//must include the silent one!!
+			o.dirs = o.dirs + w
 		}
-		//in move n torpedo my opponent cant see i m recharging my torpedo so...
+		//in move n torpedo i cant see torpedo i only see it when he fires it
 		if w == "TORPEDO" {
 			x, _ := strconv.Atoi(ord[idx+1])
 			y, _ := strconv.Atoi(ord[idx+2])
-			s.opp.torpedoPos = append(s.opp.torpedoPos, Point{x, y})
+			o.torpedoPos = append(o.torpedoPos, Point{x, y})
 		}
-		//important hint here, must find a way to parse it sector by sector
-		//the idea: if the enemy is in the SW zone and I head south,
-		//next i will go S and w and s etc..
-		//must find sth better
 		if w == "SURFACE" {
 			e, _ := strconv.Atoi(ord[idx+1])
-			s.opp.enemyZone = e
-			switch s.opp.enemyZone {
-			case 1:
-				s.me.oppSurfaceHint = "NW"
-			case 2:
-				s.me.oppSurfaceHint = "N"
-			case 3:
-				s.me.oppSurfaceHint = "NE"
-			case 4:
-				s.me.oppSurfaceHint = "CW" //C like center
-			case 5:
-				s.me.oppSurfaceHint = "C"
-			case 6:
-				s.me.oppSurfaceHint = "CE"
-			case 7:
-				s.me.oppSurfaceHint = "SW"
-			case 8:
-				s.me.oppSurfaceHint = "S"
-			case 9:
-				s.me.oppSurfaceHint = "SE"
-			}
-
+			o.surfaceZone = e
+		}
+		if w == "SONAR" {
+			e, _ := strconv.Atoi(ord[idx+1])
+			o.sonarZone = e
+		}
+		if w == "SILENCE" {
+			//to know the gaps
+			o.dirs = o.dirs + "X"
 		}
 	}
 }
+func (o *Opp) getLastTorpZone() (int, Point) {
+	var zone int
+	var lastTPos Point
+	if len(o.torpedoPos) == 1 {
+		zone = getPosBySector(o.torpedoPos[0])
+		lastTPos = o.torpedoPos[0]
+	} else {
+		zone = getPosBySector(o.torpedoPos[len(o.torpedoPos)-1])
+		lastTPos = o.torpedoPos[len(o.torpedoPos)-1]
+	}
+	o.lastTorpedoZone = zone
+	return zone, lastTPos
+}
 
-//question is: how to triangulate opp pos from his torpedoes?? BFS??
-//WRITE A GUESS FUNC TO GUESS ENEMY POS FROM HINTS!!
-
-type Turn struct {
+//ME
+type Me struct {
+	id           int
+	currentPos   Tile
+	hitPoints    int
+	visitedTiles map[Tile]bool
+	//dirs
+	canGoWest  bool
+	canGoEast  bool
+	canGoNorth bool
+	canGoSouth bool
+	//Torpedo
+	torpedoCooldown int
+	canFireTorpedo  bool
+	//Sonar
+	sonarCooldown int
+	canUseSonar   bool
+	sonarResult   string
+	//Silence
+	silenceCooldown int
+	canUseSilence   bool
+	//Opp
+	oppSurfaceHint string
+	//comm to send
 	commands []string
 }
 
-//Helpers for turn
-//false = no charge, true = charge. if multiple arm could be string?
-func (t *Turn) move(dir string, c bool) {
+func (m *Me) isTorpCharge() bool {
+	var c bool
+	if m.torpedoCooldown > 0 {
+		c = true
+	} else {
+		c = false
+		m.canFireTorpedo = true
+	}
+	return c
+}
+func (m *Me) isSonarCharge() bool {
+	var c bool
+	if m.sonarCooldown > 0 {
+		c = true
+	} else {
+		c = false
+		m.canUseSonar = true
+	}
+	return c
+}
+func (m *Me) isSilenceCharge() bool {
+	var c bool
+	if m.silenceCooldown > 0 {
+		c = true
+	} else {
+		c = false
+		m.canUseSilence = true
+	}
+	return c
+}
+func (m *Me) move(dir string) {
 	var command string
 	switch dir {
 	case "N":
@@ -161,34 +196,48 @@ func (t *Turn) move(dir string, c bool) {
 	case "E":
 		command = fmt.Sprintf("MOVE %s", dir)
 	}
-	if c {
+	//having torp charge is important but sonar?
+	//no dynamic, be charged that's all
+	if m.isTorpCharge() {
 		command = command + " TORPEDO"
 	}
-	t.commands = append(t.commands, command)
+	if !m.isTorpCharge() && m.isSonarCharge() {
+		command = command + " SONAR"
+	}
+	if !m.isTorpCharge() && !m.isSonarCharge() && m.isSilenceCharge() {
+		command = command + " SILENCE"
+	}
+	m.commands = append(m.commands, command)
 }
-func (t *Turn) surface() {
-	t.commands = append(t.commands, "SURFACE")
+func (m *Me) surface() {
+	m.commands = append(m.commands, "SURFACE")
 }
-func (t *Turn) torpedo(tile Tile) {
+func (m *Me) sonar(sector int) {
+	command := fmt.Sprintf("SONAR %d", sector)
+	m.commands = append(m.commands, command)
+}
+func (m *Me) torpedo(tile Tile) {
 	command := fmt.Sprintf("TORPEDO %d %d", tile.pos.x, tile.pos.y)
-	t.commands = append(t.commands, command)
+	m.commands = append(m.commands, command)
 }
-func (t *Turn) msg(s string) {
+func (m *Me) silence(direction string, distance int) {
+	command := fmt.Sprintf("SILENCE %s %d", direction, distance)
+	m.commands = append(m.commands, command)
+}
+func (m *Me) msg(s string) {
 	command := fmt.Sprintf("MSG %s", s)
-	t.commands = append(t.commands, command)
+	m.commands = append(m.commands, command)
 }
-
-//if and only if commands > 1
-func (t *Turn) sendTurn() {
-	if len(t.commands) == 1 {
-		fmt.Print(t.commands[0])
+func (m *Me) sendTurn() {
+	if len(m.commands) == 1 {
+		fmt.Print(m.commands[0])
 	} else {
-		fmt.Print(strings.Join(t.commands, "|"))
+		fmt.Print(strings.Join(m.commands, "|"))
 	}
 	fmt.Println()
 }
 
-//no opp nothing, goal is roaming
+//STATE
 type State struct {
 	board         string
 	carte         [HEIGHT][WIDTH]Tile
@@ -196,55 +245,24 @@ type State struct {
 	visitedTiles  map[Point]bool
 	me            Me
 	opp           Opp
-	t             Turn
-}
-
-//simple helper
-func (s *State) hasBeenVisited(t Tile) bool {
-	return s.visitedTiles[t.pos]
+	targets       []Tile
 }
 
 //YannTt'as 3 mouvements possible, tu floodfill pour chaque, garde celui qui te laisse le plus de cases dispo après move
 func (s *State) checkDirections(t Tile) {
-	//TEST
-	if t.pos.x-1 > 0 && isWalkable(s.carte[t.pos.x-1][t.pos.y]) && !s.hasBeenVisited(s.carte[t.pos.x-1][t.pos.y]) {
+	if t.pos.x-1 > 0 && isWalkable(s.carte[t.pos.x-1][t.pos.y]) && !s.me.visitedTiles[s.carte[t.pos.x-1][t.pos.y]] {
 		s.me.canGoWest = true
 	}
-	if t.pos.x+1 < WIDTH && isWalkable(s.carte[t.pos.x+1][t.pos.y]) && !s.hasBeenVisited(s.carte[t.pos.x+1][t.pos.y]) {
+	if t.pos.x+1 < WIDTH && isWalkable(s.carte[t.pos.x+1][t.pos.y]) && !s.me.visitedTiles[s.carte[t.pos.x+1][t.pos.y]] {
 		s.me.canGoEast = true
 	}
-	if t.pos.y-1 > 0 && isWalkable(s.carte[t.pos.x][t.pos.y-1]) && !s.hasBeenVisited(s.carte[t.pos.x][t.pos.y-1]) {
+	if t.pos.y-1 > 0 && isWalkable(s.carte[t.pos.x][t.pos.y-1]) && !s.me.visitedTiles[s.carte[t.pos.x][t.pos.y-1]] {
 		s.me.canGoNorth = true
 	}
-	if t.pos.y+1 < HEIGHT && isWalkable(s.carte[t.pos.x][t.pos.y+1]) && !s.hasBeenVisited(s.carte[t.pos.x][t.pos.y+1]) {
+	if t.pos.y+1 < HEIGHT && isWalkable(s.carte[t.pos.x][t.pos.y+1]) && !s.me.visitedTiles[s.carte[t.pos.x][t.pos.y+1]] {
 		s.me.canGoSouth = true
 	}
 }
-
-/*
-Flood-fill (node, target-color, replacement-color):
-  1. If target-color is equal to replacement-color, return.
-  2. If color of node is not equal to target-color, return.
-  3. Set the color of node to replacement-color.
-  4. Set Q to the empty queue.
-  5. Add node to the end of Q.
-  6. While Q is not empty:
-  7.     Set n equal to the first element of Q.
-  8.     Remove first element from Q.
-  9.     If the color of the node to the west of n is target-color,
-             set the color of that node to replacement-color and add that node to the end of Q.
- 10.     If the color of the node to the east of n is target-color,
-             set the color of that node to replacement-color and add that node to the end of Q.
- 11.     If the color of the node to the north of n is target-color,
-             set the color of that node to replacement-color and add that node to the end of Q.
- 12.     If the color of the node to the south of n is target-color,
-             set the color of that node to replacement-color and add that node to the end of Q.
- 13. Continue looping until Q is exhausted.
- 14. Return.
-
- https://rosettacode.org/wiki/Bitmap/Flood_fill#Go ==> would like to see the getpx func i imagine a getTile()
- with limit checkers in a Grid Type...
-*/
 func (s *State) floodfill(t Tile) {
 	var queue []Tile
 	queue = append(queue, t)
@@ -291,50 +309,6 @@ func (s *State) floodfill(t Tile) {
 		//log.Println(t.pos, t.color)
 	}
 	log.Println("FIN", numT)
-}
-
-//torpedo part must be moved one day!!
-func (s *State) possibleDir() {
-	var c bool
-	if s.me.torpedoCooldown <= 3 {
-		c = true
-	} else {
-		c = false
-	}
-	/*
-		//TEST, now should fire in the range!! and should chase the goose!!
-		if s.me.torpedoCooldown == 0 {
-			//this is shitty com'on!!
-			s.t.commands = append(s.t.commands, (torpedo(s.carte[s.me.currentPos.pos.x+2][s.me.currentPos.pos.y])))
-		}
-		//TEST
-		var dir string
-		if len(s.opp.torpedoPos) != 0 {
-			dir = getDirFromPoint(s.me.currentPos.pos, s.opp.torpedoPos[0])
-		}
-	*/
-	//I know...but did i grasp the logic??
-	s.checkDirections(s.me.currentPos)
-	if s.me.canGoSouth {
-		s.t.move("S", c)
-	}
-	if !s.me.canGoSouth && s.me.canGoEast {
-		s.t.move("E", c)
-	}
-	if !s.me.canGoSouth && !s.me.canGoEast && s.me.canGoNorth {
-		s.t.move("N", c)
-	}
-	if !s.me.canGoNorth && !s.me.canGoEast && !s.me.canGoSouth && s.me.canGoWest {
-		s.t.move("W", c)
-	}
-	//one direction is possible but cell has already been visited!! so surface
-	if !s.me.canGoNorth && !s.me.canGoEast && !s.me.canGoSouth && !s.me.canGoWest {
-		s.t.surface()
-		//should reset visited
-		for c, _ := range s.visitedTiles {
-			s.visitedTiles[c] = false
-		}
-	}
 }
 
 //voronoi to get possible zones??
@@ -403,9 +377,11 @@ func (s *State) getBfsPath(startPos, target Tile) []Tile {
 //!! You can't change values associated with keys in a map, you can only reassign values.
 //!! When you "fill" the map, you can't use the loop's variable, as it gets overwritten in each iteration
 // see : https://stackoverflow.com/questions/42716852/how-to-update-map-values-in-go
-func (s *State) calculateDist(src Tile) map[Tile]*int {
-	var dist = make(map[Tile]*int)
+//func (s *State) calculateDist(src Tile) map[Tile]*int {
+func (s *State) getTargets(src Tile) {
+	//var dist = make(map[Tile]*int)
 	var path []Tile
+	//should change that walkable to take into account floodfill?
 	for _, target := range s.walkableTiles {
 		//dont need this one
 		if target == src {
@@ -413,9 +389,56 @@ func (s *State) calculateDist(src Tile) map[Tile]*int {
 		}
 		path = s.getBfsPath(src, target)
 		length := len(path)
-		dist[target] = &length
+		if length >= 4 {
+			s.targets = append(s.targets, path[0])
+		}
+		//dist[target] = &length
 	}
-	return dist
+	//return dist
+}
+
+func getBestMove(s State) {
+	//it takes state as an arg, clone it to sim?
+	//determine which direction is best wrt the num of wtiles left and the opp
+	//location (indeed where we think he might be thx to the info from surface, sonar
+	//or torpedo)
+	//should be there in the end
+	//s.checkDirections(m.currentPos)
+	//determine what commands we must send so that everything gets charged + choose
+	//between torp/sonar/silence?
+}
+
+//question is: how to triangulate opp pos from his torpedoes?? BFS??
+//WRITE A GUESS FUNC TO GUESS ENEMY POS FROM HINTS!!
+//I know...but did i grasp the logic??
+func (s *State) woodMoves() {
+	if s.me.canGoSouth {
+		s.me.move("S")
+	}
+	if !s.me.canGoSouth && s.me.canGoEast {
+		s.me.move("E")
+	}
+	if !s.me.canGoSouth && !s.me.canGoEast && s.me.canGoNorth {
+		s.me.move("N")
+	}
+	if !s.me.canGoNorth && !s.me.canGoEast && !s.me.canGoSouth && s.me.canGoWest {
+		s.me.move("W")
+	}
+	//TEST
+	//if i am round the torp zone sonar to see what happens
+	if getPosBySector(s.me.currentPos.pos) == s.opp.lastTorpedoZone {
+		//s.me.sonar(s.opp.lastTorpedoZone) //useless should be able to use sonar in the neighhouring zones
+		_, p := s.opp.getLastTorpZone()
+		s.me.torpedo(s.carte[p.x][p.y])
+	}
+	//one direction is possible but cell has already been visited!! so surface
+	if !s.me.canGoNorth && !s.me.canGoEast && !s.me.canGoSouth && !s.me.canGoWest {
+		s.me.surface()
+		//should reset visited
+		for c, _ := range s.me.visitedTiles {
+			s.me.visitedTiles[c] = false
+		}
+	}
 }
 
 func main() {
@@ -437,13 +460,10 @@ func main() {
 		s.board = s.board + line
 	}
 
-	//init graph keeping board
-	//BIG TEST never did that before!!
 	for i := 0; i < HEIGHT; i++ {
 		for j := 0; j < WIDTH; j++ {
 			//red by default
 			s.carte[i][j] = Tile{Point{i, j}, string(s.board[j*WIDTH+i]), "red"}
-			//get a list of walkable cells to choose randomly a starting point
 			if s.carte[i][j].what == "." {
 				//green like "you could go there"
 				s.carte[i][j].color = "green"
@@ -455,15 +475,20 @@ func main() {
 	//my starting pos
 	var startPos = s.walkableTiles[rand.Intn(len(s.walkableTiles))]
 	fmt.Println(startPos.pos.x, startPos.pos.y)
+	//var startPos = s.carte[14][5]
 	//fmt.Println("14 5") //debug purpose
 
-	s.floodfill(startPos)
-	s.visitedTiles = make(map[Point]bool)
+	//DOES NOT WORK
+	s.getTargets(startPos)
+	//Dont use it yet so...
+	//s.floodfill(startPos)
+	s.me.visitedTiles = make(map[Tile]bool)
+	//log.Println(s.getBfsPath(startPos, s.carte[12][3]))
+
 	/*
 		dist := s.calculateDist(startPos)
-
 		//toying but could use that to calculate the nearest torpedo pos for ex
-		var max = 0
+		var max = 4
 		var farthestTile Tile
 		for k, v := range dist {
 			if *v > max {
@@ -472,40 +497,36 @@ func main() {
 			}
 		}
 
-		//log.Println("TARGET: ", farthestTile, "DIST: ", max)
+		log.Println("TARGET: ", farthestTile, "DIST: ", max)
 	*/
 	var turn int
 	for {
 		var x, y, myLife, oppLife, torpedoCooldown, sonarCooldown, silenceCooldown, mineCooldown int
 		scanner.Scan()
 		fmt.Sscan(scanner.Text(), &x, &y, &myLife, &oppLife, &torpedoCooldown, &sonarCooldown, &silenceCooldown, &mineCooldown)
-
+		//should write a New func for State no?
 		s.me.currentPos = s.carte[x][y]
-		s.visitedTiles[s.me.currentPos.pos] = true
+		s.me.visitedTiles[s.me.currentPos] = true
 		s.me.hitPoints = myLife
-		s.opp.hitPoints = oppLife
 		s.me.torpedoCooldown = torpedoCooldown
+		s.me.sonarCooldown = sonarCooldown
+		s.me.silenceCooldown = silenceCooldown
 
-		s.possibleDir()
+		s.opp.hitPoints = oppLife
+
+		s.checkDirections(s.me.currentPos)
+		s.woodMoves()
 
 		var sonarResult string
 		scanner.Scan()
 		fmt.Sscan(scanner.Text(), &sonarResult)
+		s.me.sonarResult = sonarResult
 
 		scanner.Scan()
 		opponentOrders := scanner.Text()
-		s.parseOppOrders(opponentOrders)
+		s.opp.parseOppOrders(opponentOrders)
 
-		//TEST
-		//idea: i know where my enemy is
-		//check if it's possible to go in that direction
-		//if yes must go!! and fire fire fire!!
-		if len(s.opp.torpedoPos) != 0 {
-			//log.Println(s.opp.torpedoPos)
-			s.getPosBySector(s.opp.torpedoPos[0])
-			//log.Println(s.opp.enemyZone)
-		}
-		s.t.sendTurn()
+		s.me.sendTurn()
 
 		//reset turn player data
 		//write a reset turn eventually...
@@ -514,7 +535,7 @@ func main() {
 		s.me.canGoSouth = false
 		s.me.canGoWest = false
 		s.me.canGoEast = false
-		s.t.commands = []string{}
+		s.me.commands = []string{}
 		turn += 1
 	}
 }
