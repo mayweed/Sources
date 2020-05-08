47c4f47d88383b911d56064dbb0c90e90c77abbc
Guillaume Raimondeau <raimondeau@gmail.com>
2020-05-08 17:25:27 +0200

    test the bfs now must write the getMostValPath to choose one to go

--- Go/CG/contests/springChallenge/sc.go
+++ Go/CG/contests/springChallenge/sc.go
@@ -35,8 +35,6 @@ func (g *Grid) NewGrid(c string) {
 		}
 	}
 }
-func (g *Grid) updateGrid(x, y, what int) {
-}
 
 //no pointer: Grid has a string meth, not *Grid !!
 func (g *Grid) String() string {
@@ -57,7 +55,53 @@ func (g *Grid) String() string {
 	//fmt.Println()
 	return buf.String()
 }
+func (g Grid) getNeighbours(p Pellet) []Pellet {
+	var neighbours []Pellet
+	if p.x+1 < g.w && g.c[p.x+1][p.y].what != -1 {
+		neighbours = append(neighbours, g.c[p.x+1][p.y])
+	}
+	if p.x-1 >= 0 && g.c[p.x-1][p.y].what != -1 {
+		neighbours = append(neighbours, g.c[p.x-1][p.y])
+	}
+	if p.y+1 < g.h && g.c[p.x][p.y+1].what != -1 {
+		neighbours = append(neighbours, g.c[p.x][p.y+1])
+	}
+	if p.y-1 >= 0 && g.c[p.x][p.y-1].what != -1 {
+		neighbours = append(neighbours, g.c[p.x][p.y-1])
+	}
+	return neighbours
 
+}
+func (g Grid) bfs(startPoint, endPoint Pellet) []Pellet {
+	var visited = make(map[Pellet]bool)
+	var pred = make(map[Pellet]Pellet)
+	var path []Pellet
+	var queue []Pellet
+	queue = append(queue, startPoint)
+
+	for len(queue) != 0 {
+		s := queue[0]
+		queue = queue[1:]
+		if s == endPoint {
+			path = append(path, s)
+			for pred[endPoint] != startPoint {
+				path = append(path, pred[endPoint])
+				endPoint = pred[endPoint]
+			}
+			path = append(path, startPoint)
+			break //???
+		}
+		for _, n := range g.getNeighbours(s) {
+			if !visited[n] {
+				visited[n] = true
+				queue = append(queue, n)
+				pred[n] = s
+			}
+
+		}
+	}
+	return path
+}
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Buffer(make([]byte, 1000000), 1000000)
@@ -88,6 +132,7 @@ func main() {
 		scanner.Scan()
 		fmt.Sscan(scanner.Text(), &visiblePacCount)
 
+		var myPos Pellet
 		for i := 0; i < visiblePacCount; i++ {
 			// pacId: pac number (unique within a team)
 			// mine: true if this pac is yours
@@ -97,29 +142,43 @@ func main() {
 			// speedTurnsLeft: unused in wood leagues
 			// abilityCooldown: unused in wood leagues
 			var pacId int
-			//var mine bool
-			var _mine int
+			var mine int
 			var x, y int
 			var typeId string
 			var speedTurnsLeft, abilityCooldown int
 			scanner.Scan()
-			fmt.Sscan(scanner.Text(), &pacId, &_mine, &x, &y, &typeId, &speedTurnsLeft, &abilityCooldown)
-			//mine = _mine != 0
+			fmt.Sscan(scanner.Text(), &pacId, &mine, &x, &y, &typeId, &speedTurnsLeft, &abilityCooldown)
+			if mine == 1 {
+				myPos = g.c[x][y]
+			}
 		}
 		// visiblePelletCount: all pellets in sight
 		var visiblePelletCount int
 		scanner.Scan()
 		fmt.Sscan(scanner.Text(), &visiblePelletCount)
 
+		var valuablePellets []Pellet
 		for i := 0; i < visiblePelletCount; i++ {
 			// value: amount of points this pellet is worth
 			var x, y, value int
 			scanner.Scan()
 			fmt.Sscan(scanner.Text(), &x, &y, &value)
 			g.c[x][y].what = value
+			if value == 10 {
+				valuablePellets = append(valuablePellets, g.c[x][y])
+			}
+		}
+		//must extract the most valuable and the shortest
+		//Best are those with multiple big pac
+		var paths [][]Pellet
+		for _, vp := range valuablePellets {
+			p := g.bfs(myPos, vp)
+			paths = append(paths, p)
 		}
 
-		fmt.Fprintln(os.Stderr, g.String())
+		//p := g.bfs(g.c[19][2], g.c[25][7])
+		//fmt.Fprintln(os.Stderr, g.String())
+		fmt.Fprintln(os.Stderr, myPos, len(paths), paths)
 		fmt.Println("MOVE 0 15 10") // MOVE <pacId> <x> <y>
 	}
 }
