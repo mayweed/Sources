ea88f692c3b873109bfc9eba44f7e7367fd4f9bb
Guillaume Raimondeau <raimondeau@gmail.com>
2020-05-23 17:02:47 +0200

    that idea does not work

--- Go/CG/practice/medium/ffe/floodFillEx.go
+++ Go/CG/practice/medium/ffe/floodFillEx.go
@@ -47,29 +47,50 @@ func (g *Grid) ff(c []Cell) {
 	//for each startCells you get the neighbour
 	//if that neighbour is == "." || != "#" you copy cell.what
 	var queue = c //will be startCells
+	var visited = make(map[Cell]bool)
 	for len(queue) > 0 {
 		//container list!!
 		start := queue[0]
+		visited[start] = true
 		queue = queue[1:]
 		if start.y+1 < g.h && g.c[start.x][start.y+1].what == "." {
-			g.c[start.x][start.y+1].what = g.c[start.x][start.y].what
-			queue = append(queue, g.c[start.x][start.y+1])
+			if !visited[g.c[start.x][start.y+1]] {
+				g.c[start.x][start.y+1].what = g.c[start.x][start.y].what
+				queue = append(queue, g.c[start.x][start.y+1])
+				visited[g.c[start.x][start.y+1]] = true
+			} else {
+				g.c[start.x][start.y+1].what = "+" //g.c[start.x][start.y].what
+			}
 		}
 		if start.y-1 >= 0 && g.c[start.x][start.y-1].what == "." {
-			g.c[start.x][start.y-1].what = g.c[start.x][start.y].what
-			queue = append(queue, g.c[start.x][start.y-1])
+			if !visited[g.c[start.x][start.y-1]] {
+				g.c[start.x][start.y-1].what = g.c[start.x][start.y].what
+				queue = append(queue, g.c[start.x][start.y-1])
+				visited[g.c[start.x][start.y-1]] = true
+			} else {
+				g.c[start.x][start.y-1].what = "+"
+			}
 		}
 		if start.x+1 < g.w && g.c[start.x+1][start.y].what == "." {
-			g.c[start.x+1][start.y].what = g.c[start.x][start.y].what
-			queue = append(queue, g.c[start.x+1][start.y])
+			if !visited[g.c[start.x+1][start.y]] {
+				g.c[start.x+1][start.y].what = g.c[start.x][start.y].what
+				queue = append(queue, g.c[start.x+1][start.y])
+				visited[g.c[start.x+1][start.y]] = true
+			} else {
+				g.c[start.x+1][start.y].what = "+"
+			}
 		}
 		if start.x-1 >= 0 && g.c[start.x-1][start.y].what == "." {
-			g.c[start.x-1][start.y].what = g.c[start.x][start.y].what
-			queue = append(queue, g.c[start.x-1][start.y])
+			if !visited[g.c[start.x-1][start.y]] {
+				g.c[start.x-1][start.y].what = g.c[start.x][start.y].what
+				queue = append(queue, g.c[start.x-1][start.y])
+				visited[g.c[start.x-1][start.y]] = true
+			} else {
+				g.c[start.x-1][start.y].what = "+"
+			}
 		}
 
 	}
-
 }
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
