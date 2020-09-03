76cacbfefe0fe7a4d599a06ac29ec27890521517
Guillaume Raimondeau <raimondeau@gmail.com>
2020-09-03 11:25:51 +0200

    need to find a ruby plugin for vim

--- Ruby/blownFuse.rb
+++ Ruby/blownFuse.rb
@@ -12,20 +12,22 @@ alreadySeen = {}
 inputs = gets.split(" ")
 for i in 0..(m-1)
     mx = inputs[i].to_i
-	n+=powerConsumption[mx-1]
-	if n > max
-		max=n
-	end
-	#appliance was off
-	if !alreadySeen[mx-1] 
-		alreadySeen[mx-1] = true
+    #STDERR.puts powerConsumption[mx-1]
+		#appliance was off
+	if !alreadySeen[mx] 
+	  n+=powerConsumption[i+1]
+        if n > max
+		  max=n
+	    end
+	  alreadySeen[mx] = true
 	else
-		n-=powerConsumption[mx-1]
+		n-=powerConsumption[i+1]
 	end
+    STDERR.puts n
 end
 
 # Write an answer using puts
-STDERR.puts powerConsumption,alreadySeen
+#STDERR.puts powerConsumption,alreadySeen
 
 puts "Fuse was not blown."
 puts "Maximal consumed current was XX A."
