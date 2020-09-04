560b40d71b21c2529486b356f2ac68e550394742
Guillaume Raimondeau <raimondeau@gmail.com>
2020-09-04 14:49:45 +0200

    A new one

---
+++ Ruby/hyperdual.rb
@@ -0,0 +1,12 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = gets.to_i
+n.times do
+    v, e = gets.split(" ").collect {|x| x.to_i}
+end
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "42"
