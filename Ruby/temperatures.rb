b6a0ca71522c38af7a55e9bea96ea6e5aa60f763
Guillaume Raimondeau <raimondeau@gmail.com>
2020-09-05 16:39:15 +0200

    More golfing

--- Ruby/temperatures.rb
+++ Ruby/temperatures.rb
@@ -1,12 +1,2 @@
 n = gets.to_i 
-inputs = gets.split(" ")
-t = []
-for i in 0..(n-1)
-  t << inputs[i].to_i
-end
-if t.empty?
-  puts 0
-else
-  t.sort!{|x,y| y<=>x}
-  puts t.sort_by{|t| t.abs}.first
-end
+p gets.split.map(&:to_i).sort!{|x,y|y<=>x}.sort_by{|t|t.abs}[0]||0
