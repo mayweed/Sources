ed7bb3da34e79b22a157240548799a684cb4976e
Guillaume Raimondeau <raimondeau@gmail.com>
2020-09-05 17:41:08 +0200

    So, next on…

---
+++ Ruby/CG/community/creditCard.rb
@@ -0,0 +1,12 @@
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = gets.to_i
+n.times do
+    card = gets.chomp
+end
+
+# Write an answer using puts
+# To debug: STDERR.puts "Debug messages..."
+
+puts "YES or NO"
