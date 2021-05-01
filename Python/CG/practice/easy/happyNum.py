133a6ad6c6575b76d004fe13425f2f9d673c1b2e
Guillaume Raimondeau <raimondeau@gmail.com>
2021-05-01 17:51:45 +0200

    playing around…

--- Python/CG/practice/easy/happyNum.py
+++ Python/CG/practice/easy/happyNum.py
@@ -1,24 +1,26 @@
 import sys
 import math
 
+def happy(n):
+        return sum(int(x) ** 2 for x in str(n))
+
 n = int(input())
 for i in range(n):
     x = int(input())
-    res = 0
-    attempt = 0
-    #combie0n de tentatives? 50? et on checke le dernier chiffre du dernier rés?
+
     # a string map etc…
     #recursion / map + lambda?
-    while attempt < 50:
-        print(attempt,x)
-        while x > 0:
-            if x%10 > 0:
-                y = x%10
-                res += (y**2) #does xot work if not y but x%10?
-            else:
-                res += x ** 2
-            x= x//10
-        attempt+=1
 
-    #print(res,file=sys.stderr)
-    #print(str(x) + :)) ou l’inverse…
+    res = happy(x)
+    attempt = 0
+    while attempt < 50:
+        if res == 1:
+            break
+        else:
+            res = happy(res)
+        attempt += 1
+
+    if res == 1:
+        print(x,":)")
+    else:
+        print(x,":(")
