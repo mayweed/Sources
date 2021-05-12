93d14c20d86b8b0034992272bb2b30105ccd592b
Guillaume Raimondeau <raimondeau@gmail.com>
2021-05-12 14:27:03 +0200

    toying

--- Python/CG/practice/easy/creditCardChecker.py
+++ Python/CG/practice/easy/creditCardChecker.py
@@ -9,21 +9,25 @@ card = 0
 for i in range(n):
     card = input()
 
-c = str(card).replace(' ','')
+    c = str(card).replace(' ','')
 
 
-x = len(c)
-l1,l2 = [],[]
-for i in range(x):
-    if i%2 == 0:
-        l2.append(c[i])
-        print("l2",i,card[i])
-    else:
+    x = len(c)
+    l1,l2 = [],[]
+    #from right to left
+    for i in range(x-2,0,-2):
+        print(i)
         l1.append(c[i])
-        print("l1",i,card[i])
 
-print(c)
-print(l1,l2)
+    #if i%2 == 0:
+    #    l2.append(c[i])
+    #    print("l2",i,card[i])
+    #else:
+    #    l1.append(c[i])
+    #    print("l1",i,card[i])
+
+    print(c)
+    print(l1,l2)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
