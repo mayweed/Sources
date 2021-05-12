497369ef154c55f8ee8a53ba04e56bbc2e31507c
Guillaume Raimondeau <raimondeau@gmail.com>
2021-05-12 15:48:34 +0200

    some fun during a loooonnngg afternoon…

--- Python/CG/practice/easy/creditCardChecker.py
+++ Python/CG/practice/easy/creditCardChecker.py
@@ -1,5 +1,6 @@
 import sys
 import math
+import functools
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
@@ -11,24 +12,29 @@ for i in range(n):
 
     c = str(card).replace(' ','')
 
-
     x = len(c)
     l1,l2 = [],[]
-    #from right to left
-    for i in range(x-2,0,-2):
-        print(i)
-        l1.append(c[i])
+    #from right to left and -1 to get c[0]
+    for i in range(x-2,-1,-2):
+        l1.append(int(c[i]))
+    nl1=list(map(lambda x : x*2,l1))
+    #conv string pour add les nums ensuite il doit y avoir plus concis non?
+    sum=0
+    for num in nl1:
+        for digit in str(num):
+            sum+=int(digit)
+    #print(sum)
 
-    #if i%2 == 0:
-    #    l2.append(c[i])
-    #    print("l2",i,card[i])
-    #else:
-    #    l1.append(c[i])
-    #    print("l1",i,card[i])
+    for i in range(x-1,-1,-2):
+        l2.append(int(c[i]))
+    nl2= functools.reduce(lambda a,b:a+b,l2)
 
-    print(c)
-    print(l1,l2)
+    check = sum + nl2
+    #print(c)
+    #print(check)
 # Write an answer using print
 # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-
-print("YES or NO")
+    if check%10:
+        print("NO")
+    else:
+        print("YES")
