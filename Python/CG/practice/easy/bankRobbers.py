74551d8c5947fddc34185fc26cfaaf7a16aad311
Guillaume Raimondeau <raimondeau@gmail.com>
2021-05-17 11:50:55 +0200

    work and cleaning, trying to get the robber’s turn

--- Python/CG/practice/easy/bankRobbers.py
+++ Python/CG/practice/easy/bankRobbers.py
@@ -1,16 +1,16 @@
 import sys
 import math
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-#lock combination
 r = int(input())
 v = int(input())
+
+#lock combination
+combs = []
+
 for i in range(v):
     c, n = [int(j) for j in input().split()]
-    print(c,n)
-    print((10*n)*(5**(c-n))) #10*5*5 oki??
-# Write an answer using print
-# To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    time =((10**n)*(5**(c-n))) 
+    combs.append(time)
+    print(time)
 
-print("1")
+print(combs)
