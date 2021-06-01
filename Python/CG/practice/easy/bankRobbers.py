4f86fa881862bc867056bc13a396c677ec4a88e6
Guillaume Raimondeau <raimondeau@gmail.com>
2021-06-01 15:35:16 +0200

    change workSig and else…

--- Python/CG/practice/easy/bankRobbers.py
+++ Python/CG/practice/easy/bankRobbers.py
@@ -11,10 +11,32 @@ for i in range(v):
     c, n = [int(j) for j in input().split()]
     time =((10**n)*(5**(c-n))) 
     combs.append(time)
-    #print(time)
 
+# idée: un cpteur qui déroule, une fois un vault fini on le retire de la liste etc…
+# une liste en cours
+count = 0
+#while combs not empty??
+# init vaults tested
+currVaults = []
+for i in range(r):
+    currVaults.append(combs.pop(0))
+
+print(len(combs),file=sys.stderr)
+
+while len(currVaults) != 0:
+    count+=1
+    for i in range(len(currVaults)):
+        print(len(currVaults),file=sys.stderr)
+        if currVaults[i] == count:
+            #print(i,file=sys.stderr)
+            #done with that vault…
+            currVaults.pop(i)
+            #time to grab the next one
+            if len(combs)>0:
+                currVaults.append(combs.pop(0))
+       
 #waiting the one with longest combs ends… 
-if len(combs) == r:
-    print(max(combs))
+#if len(combs) == r:
+#print(max(combs))
 
-print(r,combs,file=sys.stderr)
+        print(r,combs,currVaults,count,file=sys.stderr)
