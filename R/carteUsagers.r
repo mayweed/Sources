b28d710e13743bde8d0e6e93683a217fff9f93c4
Guillaume Raimondeau <raimondeau@gmail.com>
2022-07-08 16:00:11 +0200

    two new maps…

--- R/carteUsagers.r
+++ R/carteUsagers.r
@@ -1,14 +1,14 @@
 library("sf")
 library("mapsf")
 library("readxl")
-library("maptiles")
+#library("maptiles")
+library("png")
 
 #get everything 
 load("/home/guillaume/DONNEES_R/GEOFLA_2019_l93.RData")
 
 #xls : search for ods
-bibT <- read_excel(path="/home/guillaume/DONNEES_R/BUDGET_POP.xlsx",
-                   sheet=1,
+bibT <- read_excel(path="/home/guillaume/DONNEES_R/USAGERS_TX.xlsx", sheet=1,
                    col_names=TRUE)
 
 #all is very important for NA!!
@@ -19,12 +19,6 @@ GEOFLA_COMMUNE_2019_l93 <- merge(GEOFLA_COMMUNE_2019_l93,
                                  all.x=TRUE)
 
 x11() #use windows() or quartz() for mac
-
-#pdf(file="/home/guillaume/carteBudget.pdf",
-#    width=8,
-#    height=12,
-#    paper="a4") 
-
 
 mf_init(GEOFLA_COMMUNE_2019_l93)
 
@@ -44,23 +38,24 @@ mf_map(x=GEOFLA_DEP_2019_l93[GEOFLA_DEP_2019_l93$CODE_DEPT == 91,],
        lwd=3)
 
 # http://www.sthda.com/french/wiki/couleurs-dans-r
-#mf_map(x=GEOFLA_COMMUNE_2019_l93,
-#       col=NA,
-#       add=TRUE,
-#       lwd=2
-#)
+mf_map(x=GEOFLA_COMMUNE_2019_l93,
+       col=NA,
+       add=TRUE,
+       lwd=2
+)
+
 mf_prop_choro(
-  x = GEOFLA_COMMUNE_2019_l93, var = c("BUDGET", "RATIO"), inches = .18, 
+  x = GEOFLA_COMMUNE_2019_l93, var = c("INSCRITS", "TX"), inches = .5, 
   val_max = 90000, symbol = "circle", col_na = "grey", pal = "Viridis",
   breaks = "equal", nbreaks = 4, 
   leg_pos = c("bottomright", "topleft"),
-  leg_title = c("Budget/bibliothèque (en €)", "Budget/habitant (en €)"),
+  leg_title = c("Inscrits/bibliothèque", "Taux/habitant"),
   leg_no_data = "Données non communiquées",
   leg_frame = c(TRUE, TRUE),
   add = TRUE
 )
 
-mf_title(txt = "Budget d’acquisition 2020 (en €)")
+mf_title(txt = "Inscrits par commune")
 
 
 mf_map(x=GEOFLA_EPCI91_2019_l93,
@@ -71,7 +66,13 @@ mf_map(x=GEOFLA_EPCI91_2019_l93,
 
 mf_credits(txt="Données issues du rapport SCRIB 2020")
 
-# pour pdf
+# EXPORT
+#png("txusagers.png")
+#pdf(file="/home/guillaume/carteBudget.pdf",
+#    width=8,
+#    height=12,
+#    paper="a4") 
+
 #dev.off()
 
 #wait please!!
