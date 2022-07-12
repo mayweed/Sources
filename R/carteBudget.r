d703563c9b7e58c34640f51f6c320a115f582777
Guillaume Raimondeau <raimondeau@gmail.com>
2022-07-12 17:16:52 +0200

    end of the day

--- R/carteBudget.r
+++ R/carteBudget.r
@@ -17,21 +17,11 @@ GEOFLA_COMMUNE_2019_l93 <- merge(GEOFLA_COMMUNE_2019_l93,
                                  by.y="INSEE_COM",
                                  all.x=TRUE)
 
-bibS <- read_excel(path="/home/guillaume/DONNEES_R/BIB_SIGB.xlsx",
-                   sheet=1,
-                   col_names=TRUE)
-GEOFLA_COMMUNE_2019_l93 <- merge(GEOFLA_COMMUNE_2019_l93,
-                                 bibS,
-                                 by.x="INSEE_COM",
-                                 by.y="INSEE_COM",
-                                 all.x=TRUE)
+villeSansBib <- st_read("/home/guillaume/SHP/villeSansBib.shp")
 
 x11() #use windows() or quartz() for mac
 
-
 mf_init(GEOFLA_COMMUNE_2019_l93)
-
-#mf_background("/home/guillaume/Desktop/INET/STAGES/Stage_pro/ESSONNE/CARTES_DIAG/fondEssonne.png")
 
 mf_map(x=GEOFLA_DEP_2019_l93[GEOFLA_DEP_2019_l93$CODE_DEPT == 91,],
        col=NA,
@@ -39,35 +29,17 @@ mf_map(x=GEOFLA_DEP_2019_l93[GEOFLA_DEP_2019_l93$CODE_DEPT == 91,],
        add=TRUE,
        lwd=3)
 
-# http://www.sthda.com/french/wiki/couleurs-dans-r
 mf_map(x=GEOFLA_COMMUNE_2019_l93,
        col=NA,
        add=TRUE,
        lwd=2)
 
 # pas de bib dans la commune
-mf_typo(x =GEOFLA_COMMUNE_2019_l93,
-        var ="BIB",
-        val_order=c("0","1"),
-        #pch=c(4,26), #26 to 31 are unassigned and that does not work with NA
-        pal=c("lightgrey","white"),
-        col="black",
+mf_typo(x =villeSansBib,
+        var ="INSEE_COM",
+        pal=c("lightgrey"),
         leg_pos=NA,
         add=TRUE)
-
-mf_prop_choro(
-  x = GEOFLA_COMMUNE_2019_l93, var = c("BUDGET", "RATIO"), inches = .18, 
-  val_max = 90000, symbol = "circle", col_na = "grey", pal = "Viridis",
-  breaks = "equal", nbreaks = 4, 
-  leg_pos = c("bottomright", "topleft"),
-  leg_title = c("Budget/bibliothèque (en €)", "Budget/habitant (en €)"),
-  leg_no_data = "Données non communiquées",
-  leg_frame = c(TRUE, TRUE),
-  add = TRUE
-)
-
-mf_title(txt = "Budget d’acquisition 2020 (en €)")
-
 
 mf_map(x=GEOFLA_EPCI91_2019_l93,
        col=NA,
@@ -75,6 +47,19 @@ mf_map(x=GEOFLA_EPCI91_2019_l93,
        add=TRUE,
        lwd=3)
 
+mf_prop_choro(
+  x = GEOFLA_COMMUNE_2019_l93, var = c("BUDGET", "RATIO"), inches = .18, 
+  val_max = 90000, symbol = "circle", pal = "Viridis",
+  breaks = "equal", nbreaks = 4, 
+  leg_pos = c("bottomright", "topleft"),
+  leg_title = c("Budget/bibliothèque (en €)", "Budget/habitant (en €)"),
+  leg_no_data = "Données non communiquées",
+  add = TRUE
+)
+
+mf_title(txt = "Budget d’acquisition 2020 (en €)")
+
 mf_credits(txt="Données issues du rapport SCRIB 2020")
+
 #wait please!!
 locator(1)
