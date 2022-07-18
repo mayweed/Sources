a767215082cb720bfcd14fbd3297dcf9b0d7e407
Guillaume Raimondeau <raimondeau@gmail.com>
2022-07-18 13:35:46 +0200

    backup

--- R/carteZoneBlanche.R
+++ R/carteZoneBlanche.R
@@ -1,7 +1,6 @@
 library("sf")
 library("readxl")
 library("mapsf")
-library("tidyverse")
 
 # ne marche pas à cause de crs diff et st_transform() ne passe pas :(
 #get everything 
@@ -10,13 +9,6 @@ load("/home/guillaume/DONNEES_R/GEOFLA_2019_l93.RData")
 bibT <- read_excel(path="/home/guillaume/DONNEES_R/NO_BIB.xlsx",
                    sheet=1,
                    col_names=TRUE)
-#zb <- GEOFLA_COMMUNE_2019_l93 %>%
- #   filter merge(GEOFLA_COMMUNE_2019_l93,
-  #                               bibT,
-   #                              by.x="INSEE_COM",
-    #                             by.y="INSEE_COM",
-     #                            all.x=TRUE)
-#pas merge: filter!! pour ne garder que les lignes de geofla en zb
 
 bibC <- st_transform(st_read("/home/guillaume/SHP/adresses-des-bibliotheques-publiques.shp"),2154) 
 
@@ -25,14 +17,26 @@ bib91 <- bibC[bibC$dept == 91,]
 
 dep91 <- GEOFLA_DEP_2019_l93[GEOFLA_DEP_2019_l93$CODE_DEPT == 91,]
 
+zb <- st_read("/home/guillaume/SHP/zoneBlanche.shp")
 x11()
 
 plot(st_geometry(dep91))
 
-plot(st_geometry(GEOFLA_COMMUNE_2019_l93),add=T)
-plot(st_geometry(GEOFLA_EPCI91_2019_l93),add=T)
+mf_map(GEOFLA_COMMUNE_2019_l93)
+#plot(st_geometry(GEOFLA_COMMUNE_2019_l93),add=T)
+#plot(st_geometry(GEOFLA_EPCI91_2019_l93),add=T)
 
 buff <- st_buffer(bib91,5000)
+
 plot(st_geometry(buff),add=T)
-#plot(GEOFLA_COMMUNE_2019_l93[GEOFLA_COMMUNE_2019_l93$NO_BIB,])
+
+mf_typo(x=zb,
+        var="INSEE_COM",
+        pal="red",
+        leg_pos=NA,
+        add=T)
+
+mf_credits(txt="Réalisation: MDE - Données issues du rapport SCRIB 2020",pos="rightbottom")
+
+mf_title(txt="Villes n’ayant pas de bibliothèque dans un rayon de 5km")
 locator(1)
