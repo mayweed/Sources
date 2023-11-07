d4734cce3dab5e9fe88a7ed1f989e7a0daaf5890
RAIMONDEAU Guillaume <Raimondeaug@cps.local>
2023-11-07 17:49:34 +0100

    a basic map

--- R/RCPS/CPS/carteCPS.R
+++ R/RCPS/CPS/carteCPS.R
@@ -1,54 +1,46 @@
 library("sf")
 library("mapsf")
 library("readxl")
-library("ggpubr")
 
-#get everything 
-load("C:/Users/Raimondeaug/Documents/DONNEES_R/GEOFLA_2019_l93.RData")
-
-#xls : search for ods
-#bibT <- read_excel(path="C:/Users/Raimondeaug/Documents/DONNEES_R/STATS_NAVETTE.xlsx",
- #                  sheet=1,
-  #                 col_names=TRUE)
-
-#all is very important for NA!!
-#GEOFLA_COMMUNE_2019_l93 <- merge(GEOFLA_COMMUNE_2019_l93,
-#                                 bibT,
-#                                 by.x="INSEE_COM",
-#                                 by.y="INSEE_COM",
-#                                 all.x=TRUE)
+bibT <- st_read("C:/Users/Raimondeaug/Documents/SHP/TRANSFERT_BIBLIOTHEQUES.shp")
 
 x11() #use windows() or quartz() for mac
 
-#png("C:/Users/Raimondeaug/Documents/carteCPS.png",width=600,height=600,res=100)
+png("C:/Users/Raimondeaug/Documents/CARTES_R/CPS/carteCPS.png",width=600,height=600,res=100)
 
-target <- GEOFLA_EPCI91_2019_l93[1,]
+target <- bibT
 mf_init(target,expandBB=c(0,0.15,0,0))
-
-# http://www.sthda.com/french/wiki/couleurs-dans-r
-mf_map(x=GEOFLA_EPCI91_2019_l93[1,],
+mf_theme("candy",fg = "black", pos = "center", font = 1, tab = FALSE,bg="white")
+mf_map(x=bibT,
        col=NA,
        border="black",
        add=TRUE,
        lwd=3)
 
-mf_map(x=GEOFLA_COMMUNE_2019_l93[ GEOFLA_COMMUNE_2019_l93$EPCI == "200056232",] ,
-       col=NA,
-       border="black",
-       overlap=FALSE)
+mf_typo(x=bibT[bibT$transfert == "OUI",],
+        var="INSEE",
+        pal = c("lightgreen"),
+        leg_pos = NA,
+        add=TRUE)
 
-mf_label(x=GEOFLA_COMMUNE_2019_l93[ GEOFLA_COMMUNE_2019_l93$EPCI == "200056232",] ,
-         var="LIBGEO",
-#         font("var", face="bold"),
+mf_typo(x=bibT[bibT$transfert == "NON",],
+        var="INSEE",
+        pal = c("pink"),
+        leg_pos = NA,
+        add=TRUE)
+
+mf_label(bibT,
+         var="nom",
          cex=.6,
          overlap=FALSE)
 
+mf_legend_t(pal = c("pink","lightgreen"),
+            val=c("Bib. non-transférée","Bib. transférée"),
+            pos="bottomleft",
+            title=NA ) 
 
 mf_title(txt = "Agglo’ Paris-Saclay")
 mf_credits(txt="Réalisation: G. Raimondeau",pos="rightbottom")
 
 # pour pdf
 dev.off()
-
-#wait please!!
-locator(1)
