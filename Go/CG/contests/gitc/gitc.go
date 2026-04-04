bf1406b428f929284b8a00699c1303438f805daf
Guillaume Raimondeau <raimondeau@gmail.com>
2026-04-04 19:27:13 +0200

    Update from copilot correcting bugs…

--- Go/CG/contests/gitc/gitc.go
+++ Go/CG/contests/gitc/gitc.go
@@ -6,9 +6,11 @@ import (
 	"strings"
 )
 
-const (
-	DAMAGE_DURATION = 5
-)
+const DAMAGE_DURATION = 5
+
+// ------------------------------------------------------------
+// DATA STRUCTURES
+// ------------------------------------------------------------
 
 type Link struct {
 	from     int
@@ -23,6 +25,7 @@ type Factory struct {
 	productionRate int
 	disabled       int
 }
+
 type Troop struct {
 	id             int
 	owner          int
@@ -31,6 +34,7 @@ type Troop struct {
 	unitCount      int
 	remainingTurns int
 }
+
 type Bomb struct {
 	owner          int
 	from           int
@@ -38,25 +42,22 @@ type Bomb struct {
 	remainingTurns int
 }
 
-type Action struct {
-	actionType  string
-	from        int
-	to          int
-	cyborgCount int
-	msg         string
-}
-
+// Action builder
 type Player struct {
 	factories []Factory
 	troops    []Troop
 	bombs     []Bomb
-	action    Action
 	turn      []string
 	score     int
 }
 
+// ------------------------------------------------------------
+// PLAYER METHODS
+// ------------------------------------------------------------
+
 func (p Player) facWithMaxCyb() int {
-	var max, id int
+	max := -1
+	id := -1
 	for _, f := range p.factories {
 		if f.unitCount > max {
 			max = f.unitCount
@@ -65,8 +66,9 @@ func (p Player) facWithMaxCyb() int {
 	}
 	return id
 }
+
 func (p *Player) getScore() {
-	var total int
+	total := 0
 	for _, f := range p.factories {
 		total += f.unitCount
 	}
@@ -75,180 +77,205 @@ func (p *Player) getScore() {
 	}
 	p.score = total
 }
+
 func (p *Player) move(from, to, cyborgCount int) {
-	p.action.actionType = "MOVE"
-	p.action.from = from
-	p.action.to = to
-	p.action.cyborgCount = cyborgCount
-	p.turn = append(p.turn, fmt.Sprintf("MOVE %d %d %d", p.action.from, p.action.to, p.action.cyborgCount))
+	p.turn = append(p.turn, fmt.Sprintf("MOVE %d %d %d", from, to, cyborgCount))
 }
+
 func (p *Player) bomb(from, to int) {
-	p.action.actionType = "BOMB"
-	p.action.from = from
-	p.action.to = to
-	p.turn = append(p.turn, fmt.Sprintf("BOMB %d %d", p.action.from, p.action.to))
+	p.turn = append(p.turn, fmt.Sprintf("BOMB %d %d", from, to))
 }
+
 func (p *Player) wait() {
-	p.action.actionType = "WAIT"
-	p.turn = append(p.turn, fmt.Sprintf("WAIT"))
+	p.turn = append(p.turn, "WAIT")
 }
+
 func (p *Player) msg(msg string) {
-	p.action.actionType = "MSG"
-	p.action.msg = msg
-	p.turn = append(p.turn, fmt.Sprintf("MSG %s", p.action.msg))
+	p.turn = append(p.turn, fmt.Sprintf("MSG %s", msg))
 }
+
 func (p *Player) sendCommands() {
+	// Si rien n'a été ajouté → WAIT
 	if len(p.turn) == 0 {
 		p.wait()
-		p.msg("sending wait, turn empty")
 	}
-	cmd := strings.Join(p.turn, ";")
-	fmt.Println(cmd)
+	fmt.Println(strings.Join(p.turn, ";"))
 }
+
+// ------------------------------------------------------------
+// GAME STATE
+// ------------------------------------------------------------
 
 type State struct {
 	factoryCount     int
 	linkCount        int
 	links            []Link
 	neutralFactories []Factory
-	bombs            []Bomb
-	turn             int
 	me               Player
 	opp              Player
 }
 
-//This is ugly can't i put that in an anon func i pass on?
-func (s *State) sortLinksByDist() {
-	sort.Slice(s.links, func(i, j int) bool { return s.links[i].distance < s.links[j].distance })
-}
-func (s *State) sortFacByProd() {
-	sort.Slice(s.neutralFactories, func(i, j int) bool {
-		return s.neutralFactories[i].productionRate < s.neutralFactories[j].productionRate
-	})
-	sort.Slice(s.opp.factories, func(i, j int) bool { return s.opp.factories[i].productionRate < s.opp.factories[j].productionRate })
-	sort.Slice(s.me.factories, func(i, j int) bool { return s.me.factories[i].productionRate < s.me.factories[j].productionRate })
-}
+// ------------------------------------------------------------
+// INITIALIZATION
+// ------------------------------------------------------------
 
 func (s *State) readMap() {
-	// factoryCount: the number of factories
-	var factoryCount int
-	fmt.Scan(&factoryCount)
-	s.factoryCount = factoryCount
+	fmt.Scan(&s.factoryCount)
+	fmt.Scan(&s.linkCount)
 
-	// linkCount: the number of links between factories
-	var linkCount int
-	fmt.Scan(&linkCount)
-	s.linkCount = linkCount
+	s.links = make([]Link, 0, s.linkCount)
 
-	for i := 0; i < linkCount; i++ {
-		var factory1, factory2, distance int
-		fmt.Scan(&factory1, &factory2, &distance)
-		s.links = append(s.links, Link{factory1, factory2, distance})
+	for i := 0; i < s.linkCount; i++ {
+		var f1, f2, dist int
+		fmt.Scan(&f1, &f2, &dist)
+		s.links = append(s.links, Link{f1, f2, dist})
 	}
-	s.sortLinksByDist()
+
+	// Tri des liens par distance (utile pour heuristiques)
+	sort.Slice(s.links, func(i, j int) bool {
+		return s.links[i].distance < s.links[j].distance
+	})
 }
 
+// ------------------------------------------------------------
+// PARSE ENTITIES EACH TURN
+// ------------------------------------------------------------
+
 func (s *State) readEntity() {
-	// entityCount: the number of entities (e.g. factories and troops)
 	var entityCount int
 	fmt.Scan(&entityCount)
 
+	// ✅ Très important : reset à chaque tour
+	s.me.factories = nil
+	s.me.troops = nil
+	s.me.bombs = nil
+
+	s.opp.factories = nil
+	s.opp.troops = nil
+	s.opp.bombs = nil
+
+	s.neutralFactories = nil
+
 	for i := 0; i < entityCount; i++ {
-		var entityId int
-		var entityType string
-		var arg1, arg2, arg3, arg4, arg5 int
-		fmt.Scan(&entityId, &entityType, &arg1, &arg2, &arg3, &arg4, &arg5)
-		switch entityType {
+		var id int
+		var t string
+		var a1, a2, a3, a4, a5 int
+
+		fmt.Scan(&id, &t, &a1, &a2, &a3, &a4, &a5)
+
+		switch t {
+
 		case "FACTORY":
-			switch arg1 {
+			f := Factory{id, a1, a2, a3, a4}
+			switch a1 {
 			case 0:
-				s.neutralFactories = append(s.neutralFactories, Factory{id: entityId, owner: arg1, unitCount: arg2, productionRate: arg3})
+				s.neutralFactories = append(s.neutralFactories, f)
 			case 1:
-				s.me.factories = append(s.me.factories, Factory{id: entityId, owner: arg1, unitCount: arg2, productionRate: arg3})
+				s.me.factories = append(s.me.factories, f)
 			case -1:
-				s.opp.factories = append(s.opp.factories, Factory{id: entityId, owner: arg1, unitCount: arg2, productionRate: arg3})
+				s.opp.factories = append(s.opp.factories, f)
 			}
+
 		case "TROOP":
-			switch arg1 {
-			case 0:
-				s.me.troops = append(s.me.troops, Troop{entityId, arg1, arg2, arg3, arg4, arg5})
-			case -1:
-				s.opp.troops = append(s.opp.troops, Troop{entityId, arg1, arg2, arg3, arg4, arg5})
+			tr := Troop{id, a1, a2, a3, a4, a5}
+			if a1 == 1 {
+				s.me.troops = append(s.me.troops, tr)
+			} else if a1 == -1 {
+				s.opp.troops = append(s.opp.troops, tr)
 			}
+
 		case "BOMB":
-			switch arg1 {
-			case 1:
-				s.me.bombs = append(s.me.bombs, Bomb{arg1, arg2, arg3, arg4})
-			case -1:
-				s.opp.bombs = append(s.opp.bombs, Bomb{arg1, arg2, arg3, arg4})
+			b := Bomb{a1, a2, a3, a4}
+			if a1 == 1 {
+				s.me.bombs = append(s.me.bombs, b)
+			} else if a1 == -1 {
+				s.opp.bombs = append(s.opp.bombs, b)
 			}
 		}
 	}
+
 	s.me.getScore()
 	s.opp.getScore()
-	s.sortFacByProd()
+
+	// tri des factories par production
+	sort.Slice(s.me.factories, func(i, j int) bool {
+		return s.me.factories[i].productionRate > s.me.factories[j].productionRate
+	})
+	sort.Slice(s.opp.factories, func(i, j int) bool {
+		return s.opp.factories[i].productionRate > s.opp.factories[j].productionRate
+	})
+	sort.Slice(s.neutralFactories, func(i, j int) bool {
+		return s.neutralFactories[i].productionRate > s.neutralFactories[j].productionRate
+	})
 }
 
-func (s *State) clearState() {
-	s.me.factories = []Factory{}
-	s.me.troops = []Troop{}
-	s.me.turn = []string{}
-	s.neutralFactories = []Factory{}
-	s.opp.factories = []Factory{}
-}
+// ------------------------------------------------------------
+// HELPER : existe-t-il un lien direct ?
+// ------------------------------------------------------------
 
-//is there a link between f1 and f2?
 func (s State) linkTo(f1, f2 Factory) bool {
 	for _, l := range s.links {
-		if l.from == f1.id && l.to == f2.id {
+		if (l.from == f1.id && l.to == f2.id) || (l.to == f1.id && l.from == f2.id) {
 			return true
 		}
 	}
 	return false
 }
 
-//ALGO to get out of woods: take each of my fac with troops and move to neutral fac first
-//and then those of opp with less cyb?
-//could list all possible acttions and choose first those factories with
-//highest prod rate?
-//must define a better cyb count
-//must take the time to build factories. Sending all those cybs wear out
-//factories...
+// ------------------------------------------------------------
+// THINK (simple version)
+// ------------------------------------------------------------
+
 func (s *State) think() {
-	var needTroopsFactories []Factory
+
+	// Prend l'usine la plus forte
+	if len(s.me.factories) == 0 {
+		s.me.wait()
+		return
+	}
+
+	// Exemple simple :
+	// 1. Capturer neutres
 	for _, src := range s.me.factories {
-		//I should send troops to the factories!!
-		if src.unitCount < 5 {
-			needTroopsFactories = append(needTroopsFactories, src)
+		if src.unitCount <= 1 {
 			continue
 		}
-		if len(s.neutralFactories) != 0 {
-			for _, dest := range s.neutralFactories {
-				s.me.move(src.id, dest.id, 1)
-			}
-		} else {
-			for _, dest := range s.opp.factories {
-				s.me.move(src.id, dest.id, 1)
+
+		for _, nf := range s.neutralFactories {
+			if s.linkTo(src, nf) {
+				s.me.move(src.id, nf.id, 1)
 			}
 		}
 	}
-	//test to help does not work no? should think strat?
-	for _, dest := range needTroopsFactories {
-		for _, src := range s.me.factories {
-			if src.unitCount > 5 {
-				s.me.move(src.id, dest.id, 1)
+
+	// 2. Sinon attaquer l’ennemi
+	for _, src := range s.me.factories {
+		if src.unitCount > 1 {
+			for _, of := range s.opp.factories {
+				if s.linkTo(src, of) {
+					s.me.move(src.id, of.id, 1)
+				}
 			}
 		}
+	}
+
+	// Si aucune action → WAIT
+	if len(s.me.turn) == 0 {
+		s.me.wait()
 	}
 }
 
+// ------------------------------------------------------------
+// MAIN
+// ------------------------------------------------------------
+
 func main() {
-	board := State{}
-	board.readMap()
+	var game State
+	game.readMap()
+
 	for {
-		board.readEntity()
-		board.think()
-		board.me.sendCommands()
-		board.clearState()
+		game.readEntity()
+		game.think()
+		game.me.sendCommands()
 	}
 }
