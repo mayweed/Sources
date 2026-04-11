0201e3af3b13da8d1186859047eb7e359e902c83
Guillaume Raimondeau <raimondeau@gmail.com>
2026-04-11 11:54:50 +0200

    first steps in Lua, hard…

---
+++ Lua/latinSquares.lua
@@ -0,0 +1,24 @@
+-- Auto-generated code below aims at helping you parse
+-- the standard input according to the problem statement.
+
+for i=0,4-1 do
+    line = io.read()
+end
+
+chars = {} -- pour mettre les 4 chars trouvés
+chars = line[0]
+
+for i=0, #line do
+    
+
+end
+--[[ check which are the 4 distinct char in the grid
+if not => invalid
+else read 4 char and see which one is missing
+add the missing char to the list
+print the liste --]]
+
+-- Write an answer using print()
+io.stderr:write("%s\n", line)
+
+print("3 4 3 3")
