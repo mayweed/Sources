dbd89cb5a9de5d56a9fd220a905bbd89b753a1ef
Guillaume Raimondeau <raimondeau@gmail.com>
2026-04-11 16:46:15 +0200

    Some coding

--- Lua/latinSquares.lua
+++ Lua/latinSquares.lua
@@ -1,26 +1,25 @@
--- Auto-generated code below aims at helping you parse
--- the standard input according to the problem statement.
-
 for i=0,4-1 do
     line = io.read()
 end
 
-chars = {} -- pour mettre les 4 chars trouvés
-chars = line[1] -- begin by one in lua
+-- func chars qui retourne chars et sa longueur ?
+chars = {}
+seen = {}
 
-[[for i=2, #line do
- if line[2] not in chars
-     then add it
-     else continue
+for i=1,#line do
+    local c = line:sub(i,i)
+    local found = false
 
-end ]]
---[[ check which are the 4 distinct char in the grid
-if not => invalid
-else read 4 char and see which one is missing
-add the missing char to the list
-print the liste --]]
+    if not seen[c] then
+        seen[c] = true
+        table.insert(chars,c)
+    end
+end
 
--- Write an answer using print()
-io.stderr:write("%s\n", line)
+-- [[check row and col
 
-print("3 4 3 3")
+--]]
+
+if #chars ~= 4 then print("Invalid")end
+
+io.stderr:write(table.concat(chars, ", ") .. "\n")
