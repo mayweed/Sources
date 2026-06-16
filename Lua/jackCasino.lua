937df3e68bd90f3e417a94bbbc9bd6af0b435e9a
Guillaume <raimondeau@gmail.com>
2026-06-16 11:45:52 +0200

    it passes…

--- Lua/jackCasino.lua
+++ Lua/jackCasino.lua
@@ -8,32 +8,36 @@ for i = 0, ROUNDS - 1 do
 	if not n1 then
 		n1, str = PLAY:match("(%d+)%s+(%S+)")
 	end
+	n1 = tonumber(n1)
+	n2 = tonumber(n2)
 
 	--io.stderr:write("n1 " .. tostring(n1) .. " call " .. tostring(str) .. " n2 " .. tostring(n2) .. "\n")
 
 	--on the cash he currently has
 	local betAmount = math.ceil(CASH / 4) --Round up
-    io.stderr:write("Bet : "..betAmount.."\n")
+	--    io.stderr:write("Bet : "..betAmount.."\n")
 
 	if str == "ODD" then
 		if n1 % 2 ~= 0 then
-			CASH = CASH + (betAmount * 2)
-            io.stderr:write("Cash : "..CASH.."\n")
+			CASH = CASH + betAmount -- pas retiré betAmount de cash
+		--            io.stderr:write("Cash : "..CASH.."\n")
 		else
 			CASH = CASH - betAmount --he lost
 		end
 	elseif str == "EVEN" then
-		if n1 % 2 == 0 then
-			CASH = CASH + (betAmount * 2)
+		if n1 ~= 0 and n1 % 2 == 0 then
+			CASH = CASH + betAmount
+			--io.stderr:write("round ", i, " n1 ", n1, " cash ", CASH, "\n")
 		else
+			io.stderr:write("round ", i, " n1 ", n1, " cash ", CASH, "\n")
 			CASH = CASH - betAmount --he lost
 		end
 	elseif str == "PLAIN" then
 		if n1 == n2 then
-			CASH = CASH + (betAmount * 100)
+			CASH = CASH + (betAmount * 35)
 		else
 			CASH = CASH - betAmount
-            io.stderr:write("Round : ",i," cash : ", CASH, " bet : "..betAmount,"\n")
+			--           io.stderr:write("Round : ",i," cash : ", CASH, " bet : "..betAmount,"\n")
 		end
 	end
 end
